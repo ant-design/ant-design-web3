1725d7a6b0c2dcfe55d13552f5f8379ec35e9803
愚指导 <tingzhao.ytz@antfin.com>
2023-10-06 10:51:14 +0800

    feat: init useNFT hook and Web3ConfigProvider getNFTMetadata (#23)
    * feat: init useNFT hook and Web3ConfigProvider getNFTMetadata
    
    * docs: add NFTCarrd zh-CN doc
    
    ---------
    
    Co-authored-by: yutingzhao1991 <tingzhao.ytz@antgroup.com>

---
+++ packages/web3/src/NFTCard/demos/simple.tsx
@@ -0,0 +1,5 @@
+import { NFTCard } from '@ant-design/web3';
+
+export default () => {
+  return <NFTCard address="0xEcd0D12E21805803f70de03B72B1C162dB0898d9" id={42} />;
+};
---
+++ packages/web3/src/NFTCard/index.zh-CN.md
@@ -0,0 +1,12 @@
+---
+group:
+  title: Components
+---
+
+# NFTCard
+
+更简单地展示 NFT 图片的组件。
+
+## Simple Usage
+
+<code src="./demos/simple.tsx"></code>
