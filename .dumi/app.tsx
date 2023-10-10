727adaba60f110702f5a8b6a6da6ad76c5d74b2a
yutingzhao1991 <tingzhao.ytz@antgroup.com>
2023-10-10 10:41:02 +0800

    chore: use ethers provider

--- .dumi/app.tsx
+++ .dumi/app.tsx
@@ -1,7 +1,10 @@
 // @ts-ignore
-import { MockProvider, Web3ConfigProvider } from '../packages/web3/dist/esm'; // use the same provider with demo in dumi
+import { Web3ConfigProvider } from '../packages/web3/dist/esm'; // use the same provider with demo in dumi
+import * as ethers from 'ethers';
+// @ts-ignore
+import ProviderAdapter from '../packages/adapter-ethers/dist/esm';
 
-const provider = new MockProvider();
+const provider = new ProviderAdapter(ethers);
 
 export function rootContainer(container: React.ReactNode) {
   return <Web3ConfigProvider provider={provider}>{container}</Web3ConfigProvider>;
