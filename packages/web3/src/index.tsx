3bac538711e4b4add23ba67e598361e409c0791d
kiner-tang(文辉) <1127031143@qq.com>
2023-10-12 20:59:06 +0800

    feat: remove mock

--- packages/web3/src/index.tsx
+++ packages/web3/src/index.tsx
@@ -1,6 +1,5 @@
 export * from './connect-button';
 export * from './ConnectModal';
-export * from './MockProvider';
 export * from './Address';
 export * from './BrowserLink';
 export * from './NFTCard';
