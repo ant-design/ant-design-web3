d4d4fee66946aad10b705c6530722b4585fdbf97
kiner-tang(文辉) <1127031143@qq.com>
2023-10-14 12:16:00 +0800

    feat: optimize code

--- packages/web3/src/address/index.tsx
+++ packages/web3/src/address/index.tsx
@@ -1,63 +1,59 @@
-import { CopyOutlined } from '@ant-design/icons';
-import type { TooltipProps } from 'antd';
-import { Space, Tooltip, message } from 'antd';
-import React from 'react';
+import { CopyOutlined } from "@ant-design/icons";
+import type { TooltipProps } from "antd";
+import { Space, Tooltip, message } from "antd";
+import React from "react";
 
 export interface AddressProps {
-  ellipsis?:
-    | boolean
-    | {
-        headClip?: number;
-        tailClip?: number;
-      };
-  address?: string;
-  copyable?: boolean;
-  tooltip?: boolean | TooltipProps['title'];
+	ellipsis?:
+		| boolean
+		| {
+				headClip?: number;
+				tailClip?: number;
+		  };
+	address?: string;
+	copyable?: boolean;
+	tooltip?: boolean | TooltipProps["title"];
 }
-export interface AddressProps2 {
-  ellipsis?: boolean | { headClip?: number; tailClip?: number; };
-  address?: string;
-  copyable?: boolean;
-  tooltip?: boolean | TooltipProps['title'];
-}
-
 
 export const Address: React.FC<AddressProps> = (props) => {
-  const { ellipsis, address, copyable, tooltip } = props;
+	const { ellipsis, address, copyable, tooltip } = props;
 
-  const isEllipsis = !!ellipsis;
-  const { headClip = 6, tailClip = 4 } =
-    typeof ellipsis !== 'object'
-      ? {
-          headClip: 6,
-          tailClip: 4,
-        }
-      : ellipsis;
+	const isEllipsis = !!ellipsis;
+	const { headClip = 6, tailClip = 4 } =
+		typeof ellipsis !== "object"
+			? {
+					headClip: 6,
+					tailClip: 4,
+			  }
+			: ellipsis;
 
-  if (!address) {
-    return null;
-  }
+	if (!address) {
+		return null;
+	}
 
-  const filledAddress = address.startsWith('0x') ? address : `0x${address}`;
-  const displayTooltip = tooltip === undefined || tooltip === true ? filledAddress : tooltip;
+	const filledAddress = address.startsWith("0x") ? address : `0x${address}`;
+	const displayTooltip =
+		tooltip === undefined || tooltip === true ? filledAddress : tooltip;
 
-  return (
-    <Space>
-      <Tooltip title={displayTooltip}>
-        {isEllipsis
-          ? `${filledAddress.slice(0, headClip)}...${filledAddress.slice(-tailClip)}`
-          : filledAddress}
-      </Tooltip>
-      {copyable && (
-        <CopyOutlined
-          title="Copy Address"
-          onClick={() => {
-            navigator.clipboard.writeText(filledAddress).then(() => {
-              message.success('Address Copied');
-            });
-          }}
-        />
-      )}
-    </Space>
-  );
+	return (
+		<Space>
+			<Tooltip title={displayTooltip}>
+				{isEllipsis
+					? `${filledAddress.slice(0, headClip)}...${filledAddress.slice(
+							-tailClip,
+					  )}`
+					: filledAddress}
+			</Tooltip>
+			{copyable && (
+				<CopyOutlined
+					title="Copy Address"
+					onClick={() => {
+						navigator.clipboard.writeText(filledAddress).then(() => {
+							message.success("Address Copied");
+						});
+					}}
+				/>
+			)}
+		</Space>
+	);
 };
