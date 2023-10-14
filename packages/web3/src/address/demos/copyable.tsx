aae34fd921f04ebcbbb23e7aea1ab824d40fd97c
kiner-tang(文辉) <1127031143@qq.com>
2023-10-14 09:43:38 +0800

    feat: optimize api about address's ellipsis

--- packages/web3/src/address/demos/copyable.tsx
+++ packages/web3/src/address/demos/copyable.tsx
@@ -4,8 +4,8 @@ export default () => {
   return (
     <Address
       ellipsis={{
-        showHead: 8,
-        showTail: 6,
+        headClip: 8,
+        tailClip: 6,
       }}
       copyable
       address={'3ea2cfd153b8d8505097b81c87c11f5d05097c18'}
