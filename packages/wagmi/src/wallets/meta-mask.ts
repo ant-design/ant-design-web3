caa28bbeb0dad33aa4b7a60e4fcfd07ec29d057e
yutingzhao1991 <tingzhao.ytz@antgroup.com>
2023-11-07 11:31:28 +0800

    fix: metamask install judge

--- packages/wagmi/src/wallets/meta-mask.ts
+++ packages/wagmi/src/wallets/meta-mask.ts
@@ -1,11 +1,11 @@
-import { Wallet, metadata_MetaMask } from '@ant-design/web3-common';
-import { Connector } from 'wagmi';
+import { type Wallet, metadata_MetaMask } from '@ant-design/web3-common';
+import type { Connector } from 'wagmi';
 
 export function getWalletByConnector(connector: Connector): Wallet {
   return {
     ...metadata_MetaMask,
     hasBrowserExtensionInstalled: async () => {
-      return !!window.ethereum;
+      return !!window.ethereum?.isMetaMask;
     },
   };
 }
