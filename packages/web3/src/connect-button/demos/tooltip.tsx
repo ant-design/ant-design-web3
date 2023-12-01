6f0d23e9d5f7321d339d94bce0e3d657b1439a52
kiner-tang(文辉) <1127031143@qq.com>
2023-12-01 20:48:18 +0800

    feat: ConnectButton tooltip support format

--- packages/web3/src/connect-button/demos/tooltip.tsx
+++ packages/web3/src/connect-button/demos/tooltip.tsx
@@ -6,21 +6,25 @@ const App: React.FC = () => {
     <Space>
       <ConnectButton account={{ address: '3ea2cfd153b8d8505097b81c87c11f5d05097c18' }} tooltip />
       <ConnectButton
-        account={{ address: '3ea2cfd153b8d8505097b81c87c11f5d05097c18' }}
+        account={{ address: '3ea2cfd153b8d8505097b81c87c11f5d05097c18', name: 'Custom Title' }}
         tooltip={{
           title: 'aaaaaabbbbbbcccccc',
         }}
       />
       <ConnectButton
-        account={{ address: '3ea2cfd153b8d8505097b81c87c11f5d05097c18' }}
+        account={{ address: '3ea2cfd153b8d8505097b81c87c11f5d05097c18', name: 'UnCopyable' }}
         tooltip={{
           title: 'aaaaaabbbbbbcccccc',
           copyable: false,
         }}
       />
       <ConnectButton
-        account={{ address: '3ea2cfd153b8d8505097b81c87c11f5d05097c18' }}
+        account={{ address: '3ea2cfd153b8d8505097b81c87c11f5d05097c18', name: 'Default Formatter' }}
         tooltip={{ format: true }}
+      />
+      <ConnectButton
+        account={{ address: '3ea2cfd153b8d8505097b81c87c11f5d05097c18', name: 'Custom Formatter' }}
+        tooltip={{ format: (address) => address.slice(0, 10) }}
       />
     </Space>
   );
