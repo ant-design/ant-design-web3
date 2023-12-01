b97594f4cd1742e56dcf88c287a1865bfb7b2c99
curry <chaofei_liang@126.com>
2023-12-01 19:02:09 +0800

    feat: 增加browser-link组件的单元测试 (#140)
    Co-authored-by: liangchaofei <liangchaofei@liangchaofeideMacBook-Pro.local>

--- packages/web3/src/browser-link/__tests__/index.test.tsx
+++ packages/web3/src/browser-link/__tests__/index.test.tsx
@@ -8,4 +8,33 @@ describe('BrowserLink', () => {
       render(<BrowserLink address="0x21CDf0974d53a6e96eF05d7B324a9803735fFd3B" />),
     ).not.toThrow();
   });
+
+  it('renders address link correctly', () => {
+    const address = '0x21CDf0974d53a6e96eF05d7B324a9803735fFd3B';
+    const { baseElement } = render(<BrowserLink address={address} type="address" />);
+
+    const link = baseElement.querySelector('a');
+    expect(link).not.toBeNull();
+    expect(link?.getAttribute('href')).toBe(`https://etherscan.io/address/${address}`);
+  });
+
+  it('renders transaction link correctly', () => {
+    const transactionHash = '0xabc123def456';
+    const { baseElement } = render(<BrowserLink address={transactionHash} type="transaction" />);
+
+    const link = baseElement.querySelector('a');
+    expect(link).not.toBeNull();
+    expect(link?.getAttribute('href')).toBe(`https://etherscan.io/tx/${transactionHash}`);
+  });
+
+  it('renders custom href correctly', () => {
+    const customHref = 'https://custom-link.com';
+    const { baseElement } = render(
+      <BrowserLink address="0x21CDf0974d53a6e96eF05d7B324a9803735fFd3B" href={customHref} />,
+    );
+
+    const link = baseElement.querySelector('a');
+    expect(link).not.toBeNull();
+    expect(link?.getAttribute('href')).toBe(customHref);
+  });
 });
