820c413901b55196b83730634f300af7de210d2d
kiner-tang(文辉) <1127031143@qq.com>
2023-12-06 10:07:11 +0800

    feat: image support custom getNFTMetadata

--- packages/web3/src/nft-image/__tests__/index.test.tsx
+++ packages/web3/src/nft-image/__tests__/index.test.tsx
@@ -27,7 +27,7 @@ describe('NFTImage', () => {
     ).not.toThrow();
   });
 
-  it.only('use custom getNFTMetadata', async () => {
+  it('use custom getNFTMetadata', async () => {
     const address = '0x21CDf0974d53a6e96eF05d7B324a9803735fFd3B';
     const tokenId = BigInt(123);
 
