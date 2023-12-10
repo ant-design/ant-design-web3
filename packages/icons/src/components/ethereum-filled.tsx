2759a617afbe8dae0567c8e375ab4848f3f722d1
kiner-tang <1127031143@qq.com>
2023-12-10 18:07:28 +0800

    feat: icons support web3 className (#194)
    * feat: icons support web3 className
    
    * feat: optimize code

--- packages/icons/src/components/ethereum-filled.tsx
+++ packages/icons/src/components/ethereum-filled.tsx
@@ -2,8 +2,19 @@ import * as React from 'react';
 import AntdIcon from '@ant-design/icons';
 import { ReactComponent as SVGComponent } from '../svgs/ethereum-filled.svg';
 import { type IconBaseProps } from '@ant-design/icons/lib/components/Icon';
+import classnames from 'classnames';
+import { ConfigProvider } from 'antd';
 
-export const EthereumFilled = React.forwardRef<HTMLSpanElement, IconBaseProps>((props, ref) => (
-  <AntdIcon {...props} ref={ref} component={SVGComponent} />
-));
+export const EthereumFilled = React.forwardRef<HTMLSpanElement, IconBaseProps>((props, ref) => {
+  const { getPrefixCls } = React.useContext(ConfigProvider.ConfigContext);
+  const prefixCls = getPrefixCls('web3-icon-ethereum-filled');
+  return (
+    <AntdIcon
+      {...props}
+      className={classnames(prefixCls, props.className)}
+      ref={ref}
+      component={SVGComponent}
+    />
+  );
+});
 EthereumFilled.displayName = 'EthereumFilled';
--- packages/icons/src/components/sui-filled.tsx
+++ packages/icons/src/components/sui-filled.tsx
@@ -2,8 +2,19 @@ import * as React from 'react';
 import AntdIcon from '@ant-design/icons';
 import { ReactComponent as SVGComponent } from '../svgs/sui-filled.svg';
 import { type IconBaseProps } from '@ant-design/icons/lib/components/Icon';
+import classnames from 'classnames';
+import { ConfigProvider } from 'antd';
 
-export const SuiFilled = React.forwardRef<HTMLSpanElement, IconBaseProps>((props, ref) => (
-  <AntdIcon {...props} ref={ref} component={SVGComponent} />
-));
+export const SuiFilled = React.forwardRef<HTMLSpanElement, IconBaseProps>((props, ref) => {
+  const { getPrefixCls } = React.useContext(ConfigProvider.ConfigContext);
+  const prefixCls = getPrefixCls('web3-icon-sui-filled');
+  return (
+    <AntdIcon
+      {...props}
+      className={classnames(prefixCls, props.className)}
+      ref={ref}
+      component={SVGComponent}
+    />
+  );
+});
 SuiFilled.displayName = 'SuiFilled';
