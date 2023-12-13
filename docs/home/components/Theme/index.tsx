826ee4779a466034684fce39de3e6eca26314e0e
yutingzhao1991 <yutingzhao1991@gmial.com>
2023-12-13 16:41:42 +0800

    feat: ConnectModal export ModalPanel for HomePage

--- docs/home/components/Theme/index.tsx
+++ docs/home/components/Theme/index.tsx
@@ -1,5 +1,59 @@
 import styles from './index.module.less';
+import { ConnectModal, type Wallet } from '@ant-design/web3';
+import { metadata_MetaMask, metadata_WalletConnect } from '@ant-design/web3-assets';
 
-export default () => {
-  return <div className={styles.container}></div>;
+const walletList: Wallet[] = [
+  metadata_MetaMask,
+  {
+    ...metadata_WalletConnect,
+    getQrCode: () => {
+      return new Promise<{
+        uri: string;
+      }>((resolve) =>
+        setTimeout(
+          () =>
+            resolve({
+              uri: `https://ant.design/docs/react/migrate-less-variables-cn#avatar-%E5%A4%B4%E5%83%8F?timestamp=${Date.now()}&random=${Math.random()}`,
+            }),
+          2000,
+        ),
+      );
+    },
+  },
+  {
+    icon: 'https://xsgames.co/randomusers/avatar.php?g=pixel&key=3',
+    name: 'Test Wallet3',
+    remark: '备注3',
+    app: {
+      link: 'https://test.com/xxx',
+    },
+    getQrCode: () => {
+      return new Promise<{
+        uri: string;
+      }>((resolve) =>
+        setTimeout(
+          () =>
+            resolve({
+              uri: `https://ant.design/docs/react/migrate-less-variables-cn#avatar-%E5%A4%B4%E5%83%8F?timestamp=${Date.now()}&random=${Math.random()}`,
+            }),
+          2000,
+        ),
+      );
+    },
+  },
+];
+
+const App: React.FC = () => {
+  return (
+    <div className={styles.container}>
+      <ConnectModal.ModalPanel
+        title="Connect Wallet"
+        footer="Powered by AntChain"
+        walletList={walletList}
+        guide
+      />
+    </div>
+  );
 };
+
+export default App;
