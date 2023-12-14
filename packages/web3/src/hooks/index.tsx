904cf21c846954474bd2d40ed14aaac23cb8bb31
yutingzhao1991 <yutingzhao1991@gmial.com>
2023-12-14 10:26:32 +0800

    test: improve wagmi-provider test cov

--- packages/web3/src/hooks/index.tsx
+++ packages/web3/src/hooks/index.tsx
@@ -1,3 +1,4 @@
 export { default as useNFT } from './useNFT';
 export { default as useAccount } from './useAccount';
 export { default as useConnection } from './useConnection';
+export { default as useProvider } from './useProvider';
