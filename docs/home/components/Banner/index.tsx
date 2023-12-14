dd0ea1e34ccad2b2cb73fb4d09d4f38c08089520
lijianan <574980606@qq.com>
2023-12-14 22:12:27 +0800

    feat: [WIP] New website Banner

--- docs/home/components/Banner/index.tsx
+++ docs/home/components/Banner/index.tsx
@@ -1,12 +1,31 @@
+import React from 'react';
 import styles from './index.module.less';
 
-export default () => {
+const Banner: React.FC = () => {
   return (
     <div className={styles.container}>
       <img
+        draggable={false}
+        className={styles.bgImg}
         src="https://mdn.alipayobjects.com/huamei_mutawc/afts/img/A*x9HgRpaXSmsAAAAAAAAAAAAADlrGAQ/original"
-        alt=""
+        alt="banner"
       />
+      <div className={styles.wrap}>
+        <img
+          draggable={false}
+          className={styles.title}
+          src="https://mdn.alipayobjects.com/huamei_mutawc/afts/img/A*zaaqSqbOOrkAAAAAAAAAAAAADlrGAQ/original"
+          alt="title"
+        />
+        <div className={styles.btn}>
+          <a href="/zh-CN/guide">开始使用</a>
+          <a href="https://github.com/ant-design/ant-design-web3" target="_blank" rel="noreferrer">
+            GitHub
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
+
+export default Banner;
