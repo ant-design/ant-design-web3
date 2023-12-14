73e48e78b0f0e89c8c6dac1005c51315291bfa4a
jrr997 <zackchangjx@foxmail.com>
2023-12-14 12:58:50 +0800

    feat: dropdown

--- docs/home/components/ShowCase/Dropdown.tsx
+++ docs/home/components/ShowCase/Dropdown.tsx
@@ -1,12 +1,12 @@
 import { EthereumCircleFilled, USDTCircleFilled, WalletColorful } from '@ant-design/web3-icons';
 import styles from './index.module.less';
-import type { MenuProps } from 'antd';
+import type { MenuProps, MenuItemType } from 'antd';
 import { Button, Dropdown, Divider } from 'antd';
 import classNames from 'classnames';
 import React from 'react';
 import { CaretDownOutlined } from '@ant-design/icons';
 
-const items: MenuProps['items'] = [
+const items = [
   {
     key: '1',
     label: 'Ethereum',
@@ -23,16 +23,23 @@ const items: MenuProps['items'] = [
 export default () => {
   const [key, setKey] = React.useState('1');
 
-  const currentItem = items.find((item) => item.key === key);
+  const currentItem = items.find((item) => item.key === key)!;
+
+  const currentIcon = React.cloneElement(currentItem.icon, {
+    style: {
+      color: '#4877ea',
+      fontSize: 22,
+    },
+  });
 
   const children = (
-    <Button icon={currentItem.icon} type="text" style={{ width: 130 }}>
+    <Button icon={currentIcon} type="text" style={{ width: 130 }}>
       {currentItem.label} <CaretDownOutlined style={{ color: '#9b9da2' }} />
     </Button>
   );
 
-  const onClick: MenuProps['onClick'] = ({ key }) => {
-    setKey(key);
+  const onClick: MenuProps['onClick'] = ({ key: currentKey }) => {
+    setKey(currentKey);
   };
 
   return (
