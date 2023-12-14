91e63051268366f5f7a5f9c2899017953f885af7
kiner-tang <1127031143@qq.com>
2023-12-14 12:01:15 +0800

    test: icons test (#258)

---
+++ packages/icons/src/__tests__/iconTest.tsx
@@ -0,0 +1,19 @@
+import { globSync } from 'glob';
+import path from 'path';
+import { ComponentType } from 'react';
+
+export async function getComponents(dir: string): Promise<ComponentType[]> {
+  return new Promise(async (resolve) => {
+    const files = globSync(path.resolve(__dirname, '..', dir, '*.tsx'));
+    const res: ComponentType[] = [];
+    for (const file of files) {
+      const comp = await import(file);
+      const displayNames = Object.keys(comp);
+      displayNames.forEach((displayName) => {
+        const Icon = comp[displayName];
+        res.push(Icon);
+      });
+    }
+    resolve(res);
+  });
+}
