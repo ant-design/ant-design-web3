6eea2cc2ee93ff3642463b2ae0ab03a2475c0330
愚指导 <tingzhao.ytz@antfin.com>
2023-12-14 19:52:20 +0800

    refactor: avatar in ConnectButton (#266)
    * fix: ConnectButton avatar style detail
    
    * fix: test snapshot
    
    * chore: update cssinjs for fix tslint error
    
    ---------
    
    Co-authored-by: yutingzhao1991 <yutingzhao1991@gmial.com>

--- packages/web3/src/connect-button/style/index.ts
+++ packages/web3/src/connect-button/style/index.ts
@@ -13,8 +13,22 @@ export interface ConnectButtonToken extends Web3AliasToken {
 const genConnectButtonStyle: GenerateStyle<ConnectButtonToken> = (token) => {
   return {
     [token.componentCls]: {
-      [`${token.componentCls}-text`]: {
+      [`${token.componentCls}-content`]: {
         display: 'inline-block',
+      },
+      [`${token.componentCls}-content-inner`]: {
+        display: 'flex',
+        alignItems: 'center',
+        flexDirection: 'row',
+      },
+      [`${token.componentCls}-avatar`]: {
+        height: `${token.lineHeight}em`,
+        aspectRatio: '1 / 1',
+        [`${token.antCls}-avatar`]: {
+          height: '100%',
+          width: '100%',
+          marginTop: -2,
+        },
       },
       [`${token.componentCls}-chain-select`]: {
         display: 'inline-block',
@@ -33,6 +47,7 @@ const genConnectButtonStyle: GenerateStyle<ConnectButtonToken> = (token) => {
         },
       },
     },
+
     [`${token.componentCls}-tooltip`]: {
       [`${token.componentCls}-tooltip-title`]: {
         color: new TinyColor(token.colorWhite).setAlpha(0.65).toRgbString(),
