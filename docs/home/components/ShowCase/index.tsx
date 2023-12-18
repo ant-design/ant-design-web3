49552ed57b8a4577278ed21b19f225f323c24c03
jrr997 <zackchangjx@foxmail.com>
2023-12-18 14:18:15 +0800

    styles: fix style

--- docs/home/components/ShowCase/index.tsx
+++ docs/home/components/ShowCase/index.tsx
@@ -8,11 +8,23 @@ import Connect from './Connect';
 export default () => {
   return (
     <div className={styles.container}>
-      <Buttons />
-      <Dropdown />
-      <Tooltip />
-      <Profile />
-      <Connect />
+      <h3 className={styles.title}>Rich components, easy to choose</h3>
+      <div className={styles.desc}>
+        A large number of practical components to meet your needs, flexible customization and
+        expansion
+      </div>
+      <div className={styles.componentList}>
+        <Buttons />
+        <Dropdown />
+        <Tooltip />
+        <Profile />
+        <Connect />
+      </div>
+
+      <div className={styles.footer}>
+        <div className={styles.footerText}>Made with ❤ by</div>
+        <div className={styles.footerDesc}>ANT DESIGN</div>
+      </div>
     </div>
   );
 };
