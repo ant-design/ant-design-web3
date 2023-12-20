ba58012a7251f0209e94e522592916cb4444a714
gin-lsl <gin_lsl@outlook.com>
2023-12-20 12:42:02 +0800

    docs: fix Home banner styles

--- .dumi/app.tsx
+++ .dumi/app.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ConfigProvider } from 'antd';
 
-export function rootContainer(container: React.ReactNode) {
+export function rootContainer(container: React.ReactNode): React.ReactNode {
   return <ConfigProvider>{container}</ConfigProvider>;
 }
--- docs/home/components/Banner/index.tsx
+++ docs/home/components/Banner/index.tsx
@@ -15,19 +15,25 @@ const Banner: React.FC = () => {
         alt="banner"
       />
       <div className={styles.wrap}>
-        <img
-          draggable={false}
-          className={styles.titleImg}
-          src="https://mdn.alipayobjects.com/huamei_mutawc/afts/img/A*zaaqSqbOOrkAAAAAAAAAAAAADlrGAQ/original"
-          alt="title"
-        />
-        <div className={styles.btn}>
-          <Link to={`${intl.locale === 'zh-CN' ? '/zh-CN' : ''}/guide`}>
-            {intl.formatMessage({ id: 'app.docs.site.index.banner.button-text' })}
-          </Link>
-          <a href="https://github.com/ant-design/ant-design-web3" target="_blank" rel="noreferrer">
-            GitHub
-          </a>
+        <div>
+          <img
+            draggable={false}
+            className={styles.titleImg}
+            src="https://mdn.alipayobjects.com/huamei_mutawc/afts/img/A*zaaqSqbOOrkAAAAAAAAAAAAADlrGAQ/original"
+            alt="title"
+          />
+          <div className={styles.btn}>
+            <Link to={`${intl.locale === 'zh-CN' ? '/zh-CN' : ''}/guide`}>
+              {intl.formatMessage({ id: 'app.docs.site.index.banner.button-text' })}
+            </Link>
+            <a
+              href="https://github.com/ant-design/ant-design-web3"
+              target="_blank"
+              rel="noreferrer"
+            >
+              GitHub
+            </a>
+          </div>
         </div>
       </div>
     </div>
