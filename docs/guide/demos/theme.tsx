d6598abe0a43cebe6de441cc9bcdf5c430ec8dda
愚道 <yutingzhao1991@gmail.com>
2023-12-20 09:48:41 +0800

    docs: improve theme custom doc

---
+++ docs/guide/demos/theme.tsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import { ConnectModal, type Wallet } from '@ant-design/web3';
+import { metadata_MetaMask, metadata_WalletConnect } from '@ant-design/web3-assets';
+import { Radio, ConfigProvider, theme, Card, Space } from 'antd';
+
+const App: React.FC = () => {
+  const [mode, setMode] = React.useState<'dark' | 'light'>('dark');
+
+  const walletList: Wallet[] = [
+    {
+      ...metadata_MetaMask,
+    },
+    {
+      ...metadata_WalletConnect,
+      getQrCode: () => {
+        return new Promise<{
+          uri: string;
+        }>((resolve) =>
+          setTimeout(
+            () =>
+              resolve({
+                uri: `https://ant.design/docs/react/migrate-less-variables-cn#avatar-%E5%A4%B4%E5%83%8F?timestamp=${Date.now()}&random=${Math.random()}`,
+              }),
+            2000,
+          ),
+        );
+      },
+    },
+    {
+      icon: 'https://xsgames.co/randomusers/avatar.php?g=pixel&key=3',
+      group: 'More',
+      name: 'Test Wallet3',
+      remark: 'remark 3',
+      app: {
+        link: 'https://test.com/xxx',
+      },
+      getQrCode: () => {
+        return new Promise<{
+          uri: string;
+        }>((resolve) =>
+          setTimeout(
+            () =>
+              resolve({
+                uri: `https://ant.design/docs/react/migrate-less-variables-cn#avatar-%E5%A4%B4%E5%83%8F?timestamp=${Date.now()}&random=${Math.random()}`,
+              }),
+            2000,
+          ),
+        );
+      },
+    },
+  ];
+
+  return (
+    <Space direction="vertical">
+      <Radio.Group
+        onChange={(e) => {
+          setMode(e.target.value);
+        }}
+        value={mode}
+      >
+        <Radio.Button value="dark">Dark</Radio.Button>
+        <Radio.Button value="light">Light</Radio.Button>
+      </Radio.Group>
+      <ConfigProvider
+        theme={{
+          algorithm: mode === 'dark' ? theme.darkAlgorithm : undefined,
+        }}
+      >
+        <Card
+          bodyStyle={{
+            padding: 0,
+            maxWidth: 737,
+          }}
+        >
+          <ConnectModal.ModalPanel walletList={walletList} guide />
+        </Card>
+      </ConfigProvider>
+    </Space>
+  );
+};
+
+export default App;
--- docs/guide/theme.zh-CN.md
+++ docs/guide/theme.zh-CN.md
@@ -6,3 +6,15 @@ group: 基础
 # 主题
 
 Ant Design Web3 基于 Ant Design 研发，你可以通过配置 [Ant Design 的主题](https://ant.design/docs/react/customize-theme-cn)来设置 Ant Design Web3 的主题。
+
+## 风格切换
+
+Ant Design Web3 内置了对暗黑模式的支持，继承自 Ant Design 的配置。
+
+<code src="./demos/theme.tsx"></code>
+
+## 自定义 Design Token
+
+你也可以完全自定义 Ant Design 的 [Design Token](https://ant.design/docs/react/customize-theme-cn#%E4%BF%AE%E6%94%B9%E7%BB%84%E4%BB%B6%E5%8F%98%E9%87%8F)，我们会在 Ant Design Web3 中继承使用。如果你发现不支持或者使用有误的 Design Token，请[提交 issue](https://github.com/ant-design/ant-design-web3/issues) 告诉我们。
+
+<code src="./demos/custom-token.tsx"></code>
