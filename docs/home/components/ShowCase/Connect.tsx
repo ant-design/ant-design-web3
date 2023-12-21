e4982af6fdea9160e5360493b2e75ec37a9d899c
愚指导 <tingzhao.ytz@antfin.com>
2023-12-21 18:06:25 +0800

    docs: Index page demo use coinbase wallet (#334)

--- docs/home/components/ShowCase/Connect.tsx
+++ docs/home/components/ShowCase/Connect.tsx
@@ -1,6 +1,10 @@
 import styles from './index.module.less';
 import { ConnectModal, type Wallet } from '@ant-design/web3';
-import { metadata_MetaMask, metadata_WalletConnect } from '@ant-design/web3-assets';
+import {
+  metadata_MetaMask,
+  metadata_WalletConnect,
+  metadata_CoinbaseWallet,
+} from '@ant-design/web3-assets';
 import classNames from 'classnames';
 import s from './connect.module.less';
 import { ConfigProvider, theme } from 'antd';
@@ -17,34 +21,14 @@ const walletList: Wallet[] = [
         setTimeout(
           () =>
             resolve({
-              uri: `https://ant.design/docs/react/migrate-less-variables-cn#avatar-%E5%A4%B4%E5%83%8F?timestamp=${Date.now()}&random=${Math.random()}`,
+              uri: `https://web3.ant.design/?timestamp=${Date.now()}&random=${Math.random()}`,
             }),
           2000,
         ),
       );
     },
   },
-  {
-    icon: 'https://xsgames.co/randomusers/avatar.php?g=pixel&key=3',
-    name: 'Test Wallet3',
-    remark: '备注3',
-    app: {
-      link: 'https://test.com/xxx',
-    },
-    getQrCode: () => {
-      return new Promise<{
-        uri: string;
-      }>((resolve) =>
-        setTimeout(
-          () =>
-            resolve({
-              uri: `https://ant.design/docs/react/migrate-less-variables-cn#avatar-%E5%A4%B4%E5%83%8F?timestamp=${Date.now()}&random=${Math.random()}`,
-            }),
-          2000,
-        ),
-      );
-    },
-  },
+  metadata_CoinbaseWallet,
 ];
 
 export default () => {
