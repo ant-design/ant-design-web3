0a52078976a47b53238c8ea40a6f2189b0fdb6b1
liangchaofei <liangchaofei@liangchaofeideMacBook-Pro.local>
2023-12-21 17:04:35 +0800

    feat: 补充nfg-image组件一个单测

--- packages/web3/src/nft-image/__tests__/index.test.tsx
+++ packages/web3/src/nft-image/__tests__/index.test.tsx
@@ -44,4 +44,21 @@ describe('NFTImage', () => {
       expect(baseElement).toMatchSnapshot();
     });
   });
+
+  it('handles invalid image URL', async () => {
+    const address = '0x21CDf0974d53a6e96eF05d7B324a9803735fFd3B';
+    const tokenId = BigInt(123);
+
+    const { baseElement } = render(
+      <NFTImage
+        address={address}
+        tokenId={tokenId}
+        getNFTMetadata={() => Promise.resolve({ name: 'Test', image: 'invalid-url' })}
+      />,
+    );
+
+    const errorIndicator = baseElement.querySelector('.error-message');
+    expect(errorIndicator).toBeNull();
+    expect(baseElement).toMatchSnapshot();
+  });
 });
