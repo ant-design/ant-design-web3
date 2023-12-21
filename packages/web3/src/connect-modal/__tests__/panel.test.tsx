e9f8fd4765b7602a86bffa00524e8d25bc1e7a8b
愚指导 <tingzhao.ytz@antfin.com>
2023-12-21 11:43:43 +0800

    fix: Get wallet button not work in ConnectModal panel mode (#326)

--- packages/web3/src/connect-modal/__tests__/panel.test.tsx
+++ packages/web3/src/connect-modal/__tests__/panel.test.tsx
@@ -1,6 +1,6 @@
 import { ConnectModal } from '@ant-design/web3';
 import { groupOrder, guide, walletList } from './mock';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import { describe, it, expect, vi } from 'vitest';
 import { Grid } from 'antd';
 
@@ -60,5 +60,77 @@ describe('ConnectModal with ConnectModal', () => {
     expect(baseElement.querySelector('.ant-web3-connect-modal-more')?.getAttribute('href')).toBe(
       guide.moreLink,
     );
+  });
+
+  it('Get wallet from init route in panel mode', async () => {
+    const App = () => (
+      <ConnectModal.ModalPanel
+        title="ConnectModal"
+        footer="Powered by AntChain"
+        walletList={[
+          {
+            icon: 'https://xsgames.co/randomusers/avatar.php?g=pixel&key=0',
+            name: 'Test Wallect',
+            remark: '备注',
+            app: {
+              link: 'https://test.com/xxx',
+            },
+            group: 'Popular',
+            getQrCode: () =>
+              Promise.resolve({
+                uri: 'wc:f7a2ae968db3720de3efa7f088a3a05a746c011bb972a4dae0a61abe66632e3d@2?relay-protocol=irn&symKey=85bf278d8a4240154c449939eb047863585619c9c7acaa78657606d66c5630b3',
+              }),
+          },
+        ]}
+        mode="normal"
+      />
+    );
+    const { baseElement } = render(<App />);
+
+    expect(baseElement.querySelector('.ant-web3-connect-modal-get-btn')?.textContent).toBe(
+      'Get a Wallet',
+    );
+    fireEvent.click(baseElement.querySelector('.ant-web3-connect-modal-get-btn')!);
+
+    expect(baseElement.querySelector('.ant-list-item-meta-title')?.textContent).toBe(
+      'Test Wallect',
+    );
+    expect(baseElement.querySelector('.ant-web3-connect-modal-get-wallet-btn')?.textContent).toBe(
+      'Get',
+    );
+    fireEvent.click(baseElement.querySelector('.ant-web3-connect-modal-get-wallet-btn')!);
+
+    expect(baseElement.querySelector('.ant-web3-connect-modal-card-title')?.textContent).toBe(
+      'Test Wallect for Mobile',
+    );
+    fireEvent.click(baseElement.querySelector('.ant-web3-connect-modal-card-item')!);
+    expect(baseElement.querySelector('.ant-web3-connect-modal-qr-code-link')?.textContent).toBe(
+      'Click to go to the download page',
+    );
+    expect(
+      baseElement.querySelector('.ant-web3-connect-modal-qr-code-link')?.getAttribute('href'),
+    ).toBe('https://test.com/xxx');
+    expect(
+      baseElement.querySelector('.ant-web3-connect-modal-qr-code-tips-download')?.textContent,
+    ).toBe('Scan the QR code to download the wallet.');
+    expect(
+      baseElement.querySelector('.ant-web3-connect-modal-main-panel-header-title')?.textContent,
+    ).toBe('Download Test Wallect');
+
+    // back to getWallet
+    fireEvent.click(baseElement.querySelector('.ant-web3-connect-modal-main-panel-header-back')!);
+    expect(
+      baseElement.querySelector('.ant-web3-connect-modal-main-panel-header-title')?.textContent,
+    ).toBe('Get Test Wallect');
+
+    // back to qrCode
+    fireEvent.click(baseElement.querySelector('.ant-web3-connect-modal-main-panel-header-back')!);
+    expect(
+      baseElement.querySelector('.ant-web3-connect-modal-main-panel-header-title')?.textContent,
+    ).toBe('Get a Wallet');
+
+    // back to init
+    fireEvent.click(baseElement.querySelector('.ant-web3-connect-modal-main-panel-header-back')!);
+    expect(baseElement.querySelector('.ant-web3-connect-modal-main-panel-header-back')).toBeNull();
   });
 });
