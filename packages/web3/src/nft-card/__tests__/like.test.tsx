cae0fe0fa88e26ddb85b65ca18b811c51a68b12c
gin-lsl <gin_lsl@outlook.com>
2023-12-23 00:08:15 +0800

    test: add nft-card test

--- packages/web3/src/nft-card/__tests__/like.test.tsx
+++ packages/web3/src/nft-card/__tests__/like.test.tsx
@@ -29,4 +29,138 @@ describe('NFTCard like', () => {
     fireEvent.click(baseElement.querySelector('.ant-nft-card-like-icon')!);
     expect(likeFn).toHaveBeenCalled();
   });
+
+  it('should controlled by user when not pass likeConfig.liked', () => {
+    const likeFn = vi.fn();
+    const address = '0x21CDf0974d53a6e96eF05d7B324a9803735fFd3B';
+    const tokenId = 123;
+    const likeConfig = {
+      totalLikes: 42,
+      onLikeChange: likeFn,
+    };
+
+    const { baseElement } = render(
+      <NFTCard
+        address={address}
+        tokenId={tokenId}
+        like={likeConfig}
+        price={{
+          value: 139999n,
+        }}
+      />,
+    );
+    // Ensure the like and price elements are rendered
+    expect(baseElement.querySelector('.ant-nft-card-like-value')).toBeTruthy();
+    expect(baseElement.querySelector('.ant-web3-crypto-price-balance')).toBeTruthy();
+    fireEvent.click(baseElement.querySelector('.ant-nft-card-like-icon')!);
+    expect(likeFn).toHaveBeenCalled();
+  });
+
+  it('render tokenId when `type` is default', () => {
+    const address = '0x21CDf0974d53a6e96eF05d7B324a9803735fFd3B';
+    const tokenId = 123;
+    const likeConfig = {
+      totalLikes: 42,
+    };
+
+    const { baseElement } = render(
+      <NFTCard
+        address={address}
+        tokenId={tokenId}
+        like={likeConfig}
+        price={{
+          value: 139999n,
+        }}
+      />,
+    );
+
+    expect(
+      baseElement.querySelector('.ant-nft-card-content .ant-nft-card-serial-number'),
+    ).toBeTruthy();
+    expect(
+      baseElement.querySelector('.ant-nft-card-content .ant-nft-card-serial-number')?.textContent,
+    ).toBe('#123');
+  });
+
+  it('render tokenId when `type` is pithy', () => {
+    const address = '0x21CDf0974d53a6e96eF05d7B324a9803735fFd3B';
+    const tokenId = 123;
+    const likeConfig = {
+      totalLikes: 42,
+    };
+
+    const { baseElement } = render(
+      <NFTCard
+        address={address}
+        tokenId={tokenId}
+        like={likeConfig}
+        type="pithy"
+        price={{
+          value: 139999n,
+        }}
+      />,
+    );
+
+    expect(
+      baseElement.querySelector('.ant-nft-card-body .ant-nft-card-serial-number'),
+    ).toBeTruthy();
+    expect(
+      baseElement.querySelector('.ant-nft-card-body .ant-nft-card-serial-number')?.textContent,
+    ).toBe('No:123');
+  });
+
+  it('`tokenId` can not pass', () => {
+    const address = '0x21CDf0974d53a6e96eF05d7B324a9803735fFd3B';
+    const likeConfig = {
+      totalLikes: 42,
+    };
+
+    const { baseElement } = render(
+      <NFTCard
+        address={address}
+        like={likeConfig}
+        price={{
+          value: 139999n,
+        }}
+      />,
+    );
+
+    expect(baseElement.querySelector('.ant-nft-card-serial-number')).toBeFalsy();
+  });
+
+  it('`pithy` mode should not render tokenId', () => {
+    const address = '0x21CDf0974d53a6e96eF05d7B324a9803735fFd3B';
+    const likeConfig = {
+      totalLikes: 42,
+    };
+
+    const { baseElement } = render(
+      <NFTCard
+        address={address}
+        like={likeConfig}
+        type="pithy"
+        price={{
+          value: 139999n,
+        }}
+      />,
+    );
+
+    expect(baseElement.querySelector('.ant-nft-card-serial-number')).toBeFalsy();
+  });
+
+  it('render skeleton when loading', () => {
+    const { baseElement } = render(
+      <NFTCard
+        getNFTMetadata={() => {
+          return new Promise<any>((resolve) => {
+            setTimeout(() => {
+              resolve({});
+            }, 2000);
+          });
+        }}
+      />,
+    );
+
+    expect(baseElement.querySelector('.ant-nft-card-wrap')).toBeFalsy();
+  });
 });
