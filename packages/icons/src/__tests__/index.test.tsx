996051c963f19e52f667ad2e58a3b7316528d441
gin-lsl <gin_lsl@outlook.com>
2023-12-22 22:41:58 +0800

    test: add icon path id test

--- packages/icons/src/__tests__/index.test.tsx
+++ packages/icons/src/__tests__/index.test.tsx
@@ -10,4 +10,20 @@ describe('Test Icons', () => {
       expect(container.firstChild).toMatchSnapshot();
     });
   });
+
+  it('icon path referenced id should start with `ant-web3-icon-`', async () => {
+    const comps = await getComponents('components');
+
+    comps.forEach((Comp) => {
+      const { container } = render(<Comp />);
+
+      const pathWithFill = container.querySelectorAll<SVGPathElement>('path[fill^="url(#"]');
+
+      // The `id` referenced by the `fill` attr of the `path` element
+      // must start with `ant-web3-icon-`
+      pathWithFill.forEach((path) => {
+        expect(path.getAttribute('fill')).toMatch(/^url\(#ant-web3-icon-/);
+      });
+    });
+  });
 });
