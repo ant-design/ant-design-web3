5dc29270c5e0970a2b425cc02c52e049ae95ca86
Kylin <1159469891@qq.com>
2023-12-23 11:40:09 +0800

    fix: remove unused code

--- packages/web3/src/connect-modal/index.tsx
+++ packages/web3/src/connect-modal/index.tsx
@@ -12,7 +12,7 @@ export * from './interface';
 export const ConnectModal: React.FC<ConnectModalProps> & {
   ModalPanel: typeof ModalPanel;
 } = (props) => {
-  const { title, open, footer, onWalletSelected, className, mode, ...restProps } = props;
+  const { open, className, mode, ...restProps } = props;
   const { getPrefixCls } = React.useContext(ConfigContext);
 
   const { isSimple } = useMode(mode);
