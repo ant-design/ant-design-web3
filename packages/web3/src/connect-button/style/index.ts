51f47f06572756bf672333f61dfd3633aa445095
kiner-tang <1127031143@qq.com>
2023-12-25 14:51:14 +0800

    feat: optimize code

--- packages/web3/src/connect-button/style/index.ts
+++ packages/web3/src/connect-button/style/index.ts
@@ -31,12 +31,8 @@ const genConnectButtonStyle: GenerateStyle<ConnectButtonToken> = (token) => {
         },
       },
       [`&${token.antCls}-btn-sm`]: {
-        [`${token.componentCls}-avatar`]: {
-          [`${token.antCls}-avatar`]: {
-            height: '70%',
-            width: '70%',
-          },
-        },
+        height: `calc(${token.controlHeightSM}px + 4px)`,
+        paddingBlock: 2,
       },
       [`${token.componentCls}-chain-select`]: {
         display: 'inline-block',
