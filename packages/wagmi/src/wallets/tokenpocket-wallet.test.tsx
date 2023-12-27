b8f3e8e662da2f96cbc773d6e72172aba4d80d6b
lk419649 <lk419649@digital-engine.com>
2023-12-27 20:52:29 +0800

    fix: Supplementary unit tests

--- packages/wagmi/src/wallets/tokenpocket-wallet.test.tsx
+++ packages/wagmi/src/wallets/tokenpocket-wallet.test.tsx
@@ -7,6 +7,12 @@ describe('TokenPocket Wallet', () => {
   it('name', async () => {
     expect(wallet.name).toBe('TokenPocket Wallet');
   });
+
+  it('always return true for hasWalletReady', async () => {
+    const install = await wallet.hasWalletReady?.();
+    expect(install).toBe(true);
+  });
+
   it('should create a wallet with the correct properties', () => {
     for (const key in wallet) {
       expect(wallet).toHaveProperty(key);
