630ea00f86d6c4f3a07bf6353ad11d98bab47807
kiner-tang(文辉) <1127031143@qq.com>
2023-12-28 22:15:44 +0800

    feat: reset

--- packages/web3/src/nft-card/__tests__/action.test.tsx
+++ packages/web3/src/nft-card/__tests__/action.test.tsx
@@ -25,25 +25,4 @@ describe('NFTCard actions', () => {
     fireEvent.click(baseElement.querySelector('.ant-nft-card-action .ant-btn')!);
     expect(actionFn).toHaveBeenCalled();
   });
-  it('render action with default actionText', () => {
-    const actionFn = vi.fn();
-    const address = '0x21CDf0974d53a6e96eF05d7B324a9803735fFd3B';
-    const tokenId = 123;
-
-    const { baseElement } = render(
-      <NFTCard
-        address={address}
-        tokenId={tokenId}
-        showAction
-        onActionClick={actionFn}
-        price={{
-          value: 139999n,
-        }}
-      />,
-    );
-    // Ensure the like and price elements are rendered
-    expect(baseElement.querySelector('.ant-nft-card-action')?.textContent).toBe('Buy Now');
-    fireEvent.click(baseElement.querySelector('.ant-nft-card-action .ant-btn')!);
-    expect(actionFn).toHaveBeenCalled();
-  });
 });
