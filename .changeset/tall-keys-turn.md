c549a0966eb2371161eb8691829d5fdc2ad8f37e
MangoGoing <37927942+cong1223@users.noreply.github.com>
2024-01-02 17:10:44 +0800

    feat: Add commonly used icons for Celo, Aave, Moonbeam, and more. (#441)
    * feat: Add commonly used icons for Celo, Aave, Moonbeam, and more.
    
    * chore: test update
    
    * chore: Rename svg classname
    
    * chore: test update
    
    ---------
    
    Co-authored-by: cong_wang <congwang@blocksec.com>

---
+++ .changeset/tall-keys-turn.md
@@ -0,0 +1,5 @@
+---
+'@ant-design/web3-icons': minor
+---
+
+Add commonly used icons for Celo, Aave, Moonbeam, and more.
---
+++ packages/icons/src/components/pancakeswap-colorful.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import AntdIcon from '@ant-design/icons';
+import SVGComponent from '../svgs/pancakeswap-colorful.svg';
+import { type IconBaseProps } from '@ant-design/icons/lib/components/Icon';
+import classnames from 'classnames';
+import { ConfigProvider } from 'antd';
+
+export const PancakeSwapColorful = React.forwardRef<HTMLSpanElement, IconBaseProps>(
+  (props, ref) => {
+    const { getPrefixCls } = React.useContext(ConfigProvider.ConfigContext);
+    const prefixCls = getPrefixCls('web3-icon-pancakeswap-colorful');
+    return (
+      <AntdIcon
+        {...props}
+        className={classnames(prefixCls, props.className)}
+        ref={ref}
+        component={SVGComponent}
+      />
+    );
+  },
+);
+PancakeSwapColorful.displayName = 'PancakeSwapColorful';
