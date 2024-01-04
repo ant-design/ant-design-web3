007ec10a2ac96d736645c3a8c273c469ee2b2311
tingzhao.ytz <yutingzhao1991@gmail.com>
2024-01-04 15:54:12 +0800

    test: add custom wallet test

--- packages/wagmi/src/wagmi-provider/__tests__/config-with-universal-wallet.test.tsx
+++ packages/wagmi/src/wagmi-provider/__tests__/config-with-universal-wallet.test.tsx
@@ -1,6 +1,6 @@
-import EventEmitter from 'events';
-import { Connector, useProvider, type ConnectorTriggerProps } from '@ant-design/web3';
+import { useProvider } from '@ant-design/web3';
 import {
+  metadata_MetaMask,
   metadata_TokenPocket,
   UniversalWallet,
   WagmiWeb3ConfigProvider,
@@ -62,5 +62,90 @@ describe('WagmiWeb3ConfigProvider with UniversalWallet', () => {
     );
     const { baseElement } = render(<App />);
     expect(baseElement.querySelector('.wallets-name')?.textContent).toBe('MetaMask,TokenPocket');
+  });
+
+  it('custom wallet', () => {
+    const chains = [polygon, mainnet];
+    const { publicClient } = configureChains(chains, [publicProvider()]);
+
+    const config = createConfig({
+      autoConnect: true,
+      publicClient,
+      connectors: [
+        new MetaMaskConnector(),
+        new WalletConnectConnector({
+          chains,
+          options: {
+            showQrModal: false,
+            projectId: 'YOUR_WALLET_CONNET_PROJECT_ID',
+          },
+        }),
+        new InjectedConnector({
+          chains,
+          options: {
+            name: 'TokenPocket',
+            getProvider: () => (window as any).tokenpocket?.ethereum,
+          },
+        }),
+        new InjectedConnector({
+          chains,
+          options: {
+            name: 'TestWallet',
+            getProvider: () => (window as any).testWallet,
+          },
+        }),
+      ],
+    });
+
+    const CustomConnector: React.FC = () => {
+      const { availableWallets, connect } = useProvider();
+      return (
+        <div
+          className="wallets-name"
+          onClick={() => {
+            connect?.();
+          }}
+        >
+          {availableWallets?.map((item) => `${item.name}-${item.group}`).join(',')}
+        </div>
+      );
+    };
+
+    const App = () => (
+      <WagmiWeb3ConfigProvider
+        assets={[
+          new UniversalWallet({
+            ...metadata_TokenPocket,
+            group: 'Popular',
+          }),
+          {
+            name: 'TestWallet',
+            create: () => {
+              return {
+                name: 'TestWallet',
+                remark: 'My TestWallet',
+                hasWalletReady: async () => {
+                  return !!(window as any).testWallet;
+                },
+                hasExtensionInstalled: async () => {
+                  return !!(window as any).testWallet;
+                },
+              };
+            },
+          },
+          new UniversalWallet({
+            ...metadata_MetaMask,
+            group: 'More',
+          }),
+        ]}
+        config={config}
+      >
+        <CustomConnector />
+      </WagmiWeb3ConfigProvider>
+    );
+    const { baseElement } = render(<App />);
+    expect(baseElement.querySelector('.wallets-name')?.textContent).toBe(
+      'TokenPocket-Popular,TestWallet-undefined,MetaMask-More',
+    );
   });
 });
