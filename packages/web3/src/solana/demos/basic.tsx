7f9e012b1f7e5f20d6501207c1c98f6709968e14
gin-lsl <gin_lsl@outlook.com>
2024-01-08 21:47:20 +0800

    docs: del solana basic demo

--- packages/web3/src/solana/demos/basic.tsx
+++ packages/web3/src/solana/demos/basic.tsx
@@ -1,23 +1,5 @@
-import { ConnectButton, Connector } from '@ant-design/web3';
-import { Solana, SolanaWeb3ConfigProvider } from '@ant-design/web3-solana';
-import { CoinbaseWallet } from '@ant-design/web3-wagmi';
-import { CoinbaseWalletAdapter, PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
-
 const App: React.FC = () => {
-  return (
-    <SolanaWeb3ConfigProvider
-      connectionEndpoint={`https://api.mainnet-beta.solana.com/`}
-      wallets={[new CoinbaseWalletAdapter(), new PhantomWalletAdapter()]}
-      assets={[Solana, CoinbaseWallet]}
-      autoConnect
-    >
-      <div>
-        <Connector>
-          <ConnectButton addressPrefix={false} />
-        </Connector>
-      </div>
-    </SolanaWeb3ConfigProvider>
-  );
+  return <div>TODO</div>;
 };
 
 export default App;
