d4c8448800c69948cace66b0625b393f39e8412c
gin-lsl <gin_lsl@outlook.com>
2024-01-16 20:47:26 +0800

    test: add solana test cases

---
+++ packages/solana/src/solana-provider/__tests__/basic.test.tsx
@@ -0,0 +1,245 @@
+import { useState, type FC, type PropsWithChildren } from 'react';
+import { Connector, useProvider, type ConnectorTriggerProps } from '@ant-design/web3';
+import type { ConnectionContextState } from '@solana/wallet-adapter-react';
+import { fireEvent, render } from '@testing-library/react';
+import { afterAll, beforeEach, describe, expect, it, vi } from 'vitest';
+
+import { solana } from '../../chains';
+import { SolanaWeb3ConfigProvider } from '../index';
+import { xrender } from './utils';
+
+type TestConnection = Partial<ConnectionContextState['connection']>;
+
+describe('SolanaWeb3ConfigProvider', () => {
+  beforeEach(() => {
+    vi.resetAllMocks();
+  });
+
+  afterAll(() => {
+    vi.resetModules();
+  });
+
+  const mockedData = vi.hoisted(() => {
+    const mockAddress = '8dQE449ozUAS2XPyvao6hEpkAtGALo1A1q4TApayFfCo';
+    const mockAddressShort = '8dQE44...FfCo';
+    const balance = 10002;
+
+    const mockedDisconnect = vi.fn();
+
+    return {
+      address: {
+        value: mockAddress,
+        short: mockAddressShort,
+      },
+      balance,
+      mockedDisconnect,
+    };
+  });
+
+  vi.mock('@solana/wallet-adapter-react', async () => {
+    const originModules = await vi.importActual('@solana/wallet-adapter-react');
+    const { remember } = await import('./utils');
+    const mfjs =
+      // eslint-disable-next-line @typescript-eslint/consistent-type-imports
+      await vi.importActual<typeof import('@metaplex-foundation/js')>('@metaplex-foundation/js');
+    const PublicKey = mfjs.PublicKey;
+
+    const publicKey = new PublicKey(mockedData.address.value);
+
+    const ConnectionProvider: React.FC<React.PropsWithChildren<{ endpoint: string }>> = ({
+      children,
+      endpoint,
+    }) => (
+      <div>
+        <div className="endpoint">{endpoint}</div>
+        {children}
+      </div>
+    );
+    const WalletProvider: React.FC<React.PropsWithChildren> = ({ children }) => <>{children}</>;
+
+    const connectedRef = remember(false);
+    const currentWalletRef = remember<any>(null);
+
+    return {
+      ...originModules,
+      useWallet: () => {
+        // provide a state to emit re-render
+        const [, setConnected] = useState(connectedRef.value);
+        const [, setCurrentWallet] = useState(currentWalletRef.value);
+
+        return {
+          publicKey,
+          connected: connectedRef.value,
+          connect: () => {
+            connectedRef.value = true;
+            setConnected(true);
+          },
+          select: (_wallet: any) => {
+            currentWalletRef.value = _wallet;
+            setCurrentWallet(_wallet);
+          },
+          disconnect: () => {
+            mockedData.mockedDisconnect();
+          },
+          wallet: currentWalletRef.value,
+        };
+      },
+      useConnection: () => {
+        const connection: TestConnection = {
+          getBalance: async (_publicKey: any) => {
+            return mockedData.balance;
+          },
+        };
+
+        return { connection };
+      },
+      ConnectionProvider,
+      WalletProvider,
+    };
+  });
+
+  it('mount correctly', () => {
+    const App = () => (
+      <SolanaWeb3ConfigProvider>
+        <div className="content">test</div>
+      </SolanaWeb3ConfigProvider>
+    );
+
+    const { baseElement } = render(<App />);
+    expect(baseElement.querySelector('.content')?.textContent).toBe('test');
+  });
+
+  it('available custom trigger', () => {
+    const CustomButton: FC<PropsWithChildren<ConnectorTriggerProps>> = (props) => {
+      const { chain, onSwitchChain } = props;
+
+      return (
+        <div
+          onClick={() => {
+            onSwitchChain?.(solana);
+          }}
+          className="content"
+        >
+          {chain?.name}
+        </div>
+      );
+    };
+
+    const switchChain = vi.fn();
+
+    const App = () => (
+      <SolanaWeb3ConfigProvider>
+        <Connector switchChain={switchChain}>
+          <CustomButton />
+        </Connector>
+      </SolanaWeb3ConfigProvider>
+    );
+
+    // const { baseElement } = render(<App />);
+    // expect(baseElement.querySelector('.content')?.textContent).toBe('Solana');
+  });
+
+  it('available custom rpcProvider by function', () => {
+    const mockRpcEndpoint = 'https://fake-api.mainnet-beta.solana.com/';
+    const mockRpcProvider = vi.fn(() => mockRpcEndpoint);
+
+    const App = () => (
+      <SolanaWeb3ConfigProvider rpcProvider={mockRpcProvider}>
+        <div className="content">test</div>
+      </SolanaWeb3ConfigProvider>
+    );
+
+    const { selector } = xrender(App);
+    expect(selector('.content')?.textContent).toBe('test');
+
+    expect(mockRpcProvider).toBeCalled();
+  });
+
+  it('ConnectionProvider', () => {
+    const App = () => (
+      <SolanaWeb3ConfigProvider rpcProvider={'fake'}>
+        <div className="content">test</div>
+      </SolanaWeb3ConfigProvider>
+    );
+
+    const { selector } = xrender(App);
+    expect(selector('.endpoint')?.textContent).toBe('');
+  });
+
+  it('available show account address', async () => {
+    const { useWallet } = await import('@solana/wallet-adapter-react');
+    const connectRunned = vi.fn();
+
+    const Address: React.FC = () => {
+      const { account } = useProvider();
+      return <div className="address">{account?.address}</div>;
+    };
+
+    const App = () => {
+      const { connect } = useWallet();
+      return (
+        <SolanaWeb3ConfigProvider>
+          <div>
+            <div className="content">test</div>
+            <button
+              className="connect"
+              onClick={async () => {
+                await connect();
+                connectRunned();
+              }}
+            >
+              Connect
+            </button>
+            <Address />
+          </div>
+        </SolanaWeb3ConfigProvider>
+      );
+    };
+
+    const { selector } = xrender(App);
+    expect(selector('.content')?.textContent).toBe('test');
+
+    const connectBtn = selector('.connect')!;
+    const address = selector('.address');
+
+    expect(connectBtn).not.toBeNull();
+
+    // default address is empty
+    expect(address?.textContent).toBe('');
+
+    fireEvent.click(connectBtn);
+
+    await vi.waitFor(() => {
+      expect(connectRunned).toBeCalled();
+      expect(address?.textContent).toBe(mockedData.address.value);
+    });
+  });
+
+  it('available disconnect', () => {
+    const CustomConnector: FC = () => {
+      const { disconnect } = useProvider();
+      return (
+        <div>
+          <button onClick={async () => await disconnect?.()}>Disconnect</button>
+        </div>
+      );
+    };
+
+    const App: FC = () => {
+      return (
+        <SolanaWeb3ConfigProvider>
+          <CustomConnector />
+        </SolanaWeb3ConfigProvider>
+      );
+    };
+
+    const { selector } = xrender(App);
+
+    const btn = selector('button')!;
+
+    expect(btn?.textContent).toBe('Disconnect');
+    fireEvent.click(btn);
+
+    expect(mockedData.mockedDisconnect).toBeCalled();
+  });
+});
---
+++ packages/solana/src/solana-provider/__tests__/switch-wallet.test.tsx
@@ -0,0 +1,255 @@
+import EventEmitter from 'node:events';
+import React, { useEffect, useState } from 'react';
+import { useProvider } from '@ant-design/web3';
+import {
+  metadata_CoinbaseWallet,
+  metadata_Phantom,
+  Solana,
+  SolanaDevnet,
+} from '@ant-design/web3-assets';
+import { WalletReadyState, type Adapter } from '@solana/wallet-adapter-base';
+import type { Wallet } from '@solana/wallet-adapter-react';
+import { CoinbaseWalletAdapter, PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
+import { fireEvent } from '@testing-library/react';
+import { afterAll, describe, expect, it, vi } from 'vitest';
+
+import { solana, solanaDevnet } from '../../chains';
+import { WalletFactory } from '../../wallets/factory';
+import { AntDesignWeb3ConfigProvider } from '../config-provider';
+import { xrender } from './utils';
+
+const mockFn = vi.fn();
+const mockSelectWalletFn = vi.fn();
+
+const mockedData = vi.hoisted(() => {
+  const mockAddress = '8dQE449ozUAS2XPyvao6hEpkAtGALo1A1q4TApayFfCo';
+  const mockAddressShort = '8dQE44...FfCo';
+
+  return {
+    address: {
+      value: mockAddress,
+      short: mockAddressShort,
+    },
+  };
+});
+
+const event = new EventEmitter();
+
+vi.mock('@solana/wallet-adapter-react', async () => {
+  const originModules = await vi.importActual('@solana/wallet-adapter-react');
+  const mfjs =
+    // eslint-disable-next-line @typescript-eslint/consistent-type-imports
+    await vi.importActual<typeof import('@metaplex-foundation/js')>('@metaplex-foundation/js');
+  const PublicKey = mfjs.PublicKey;
+
+  const publicKey = new PublicKey(mockedData.address.value);
+
+  const ConnectionProvider: React.FC<React.PropsWithChildren<{ endpoint: string }>> = ({
+    children,
+    endpoint,
+  }) => (
+    <div>
+      <div className="endpoint">{endpoint}</div>
+      {children}
+    </div>
+  );
+  const WalletProvider: React.FC<React.PropsWithChildren> = ({ children }) => (
+    <div className="mocked-walletprovider">{children}</div>
+  );
+
+  const adapters: Adapter[] = [new CoinbaseWalletAdapter(), new PhantomWalletAdapter()];
+
+  return {
+    ...originModules,
+    useWallet: () => {
+      const [currentWallet, setCurrentWallet] = useState<Wallet>(() => {
+        return { adapter: adapters[0], readyState: WalletReadyState.Installed };
+      });
+
+      useEffect(() => {
+        event.on('walletChange', (c: string) => {
+          const foundAdapter = adapters.find((w) => w.name === c)!;
+          setCurrentWallet({
+            adapter: foundAdapter,
+            readyState: WalletReadyState.Installed,
+          });
+        });
+        return () => {
+          event.removeAllListeners('walletChange');
+        };
+      });
+      return {
+        publicKey,
+        connected: false,
+        wallet: currentWallet,
+        connect: () => {},
+        select: (walletName: string) => {
+          event.emit('walletChange', walletName);
+          mockSelectWalletFn(walletName);
+        },
+        disconnect: () => {},
+      };
+    },
+    ConnectionProvider,
+    WalletProvider,
+  };
+});
+
+describe('AntDesignWeb3ConfigProvider wallet', () => {
+  afterAll(() => {
+    mockFn.mockClear();
+    vi.unmock('@ant-design/web3-common');
+  });
+
+  it('switchWallet', async () => {
+    const { WalletProvider, useWallet } = await import('@solana/wallet-adapter-react');
+
+    const CustomConnector: React.FC = () => {
+      const { switchWallet, availableWallets } = useProvider();
+      const { wallet } = useWallet();
+
+      return (
+        <div className="customconnector">
+          <button
+            className="btn-switchwallet"
+            onClick={() => {
+              switchWallet?.(availableWallets?.[1]);
+            }}
+          >
+            Switch Wallet
+          </button>
+          <div className="current-wallet">{wallet?.adapter?.name}</div>
+        </div>
+      );
+    };
+
+    const App = () => {
+      return (
+        <WalletProvider wallets={[]}>
+          <AntDesignWeb3ConfigProvider
+            availableChains={[solana]}
+            chainAssets={[Solana]}
+            availableWallets={[
+              WalletFactory(new CoinbaseWalletAdapter(), metadata_CoinbaseWallet).create(),
+              WalletFactory(new PhantomWalletAdapter(), metadata_Phantom).create(),
+            ]}
+          >
+            <CustomConnector />
+          </AntDesignWeb3ConfigProvider>
+        </WalletProvider>
+      );
+    };
+
+    const { selector } = xrender(App);
+
+    const btn = selector('.btn-switchwallet')!;
+    expect(btn?.textContent).toBe('Switch Wallet');
+
+    const shownCurrentWallet = selector('.current-wallet');
+
+    fireEvent.click(btn);
+
+    await vi.waitFor(() => {
+      expect(shownCurrentWallet?.textContent).toBe(metadata_Phantom.name);
+    });
+  });
+
+  it('switchWallet without wallet', async () => {
+    const { WalletProvider } = await import('@solana/wallet-adapter-react');
+
+    const CustomConnector: React.FC = () => {
+      const { switchWallet } = useProvider();
+
+      return (
+        <div className="customconnector">
+          <button
+            className="btn-switchwallet"
+            onClick={() => {
+              switchWallet?.();
+            }}
+          >
+            Switch Wallet
+          </button>
+        </div>
+      );
+    };
+
+    const App = () => {
+      return (
+        <WalletProvider wallets={[]}>
+          <AntDesignWeb3ConfigProvider
+            availableChains={[solana]}
+            chainAssets={[Solana]}
+            availableWallets={[
+              WalletFactory(new CoinbaseWalletAdapter(), metadata_CoinbaseWallet).create(),
+              WalletFactory(new PhantomWalletAdapter(), metadata_Phantom).create(),
+            ]}
+          >
+            <CustomConnector />
+          </AntDesignWeb3ConfigProvider>
+        </WalletProvider>
+      );
+    };
+
+    const { selector } = xrender(App);
+
+    const btn = selector('.btn-switchwallet')!;
+    expect(btn?.textContent).toBe('Switch Wallet');
+
+    fireEvent.click(btn);
+
+    await vi.waitFor(() => {
+      expect(mockSelectWalletFn).toBeCalledWith(null);
+    });
+  });
+
+  it('switchChain', async () => {
+    const { WalletProvider, useWallet } = await import('@solana/wallet-adapter-react');
+
+    const CustomConnector: React.FC = () => {
+      const { switchChain } = useProvider();
+      const { wallet, connected } = useWallet();
+
+      return (
+        <div className="customconnector">
+          <button
+            className="btn-switchchain"
+            onClick={() => {
+              switchChain?.(solanaDevnet);
+            }}
+          >
+            Switch Chain
+          </button>
+          <div className="current-chain">{connected}</div>
+        </div>
+      );
+    };
+
+    const App: React.FC = () => {
+      return (
+        <WalletProvider wallets={[]}>
+          <AntDesignWeb3ConfigProvider
+            availableChains={[solana, solanaDevnet]}
+            chainAssets={[Solana, SolanaDevnet]}
+            availableWallets={[
+              WalletFactory(new CoinbaseWalletAdapter(), metadata_CoinbaseWallet).create(),
+              WalletFactory(new PhantomWalletAdapter(), metadata_Phantom).create(),
+            ]}
+          >
+            <CustomConnector />
+          </AntDesignWeb3ConfigProvider>
+        </WalletProvider>
+      );
+    };
+
+    const { selector } = xrender(App);
+
+    const btn = selector('.btn-switchchain')!;
+    const shownCurrentChain = selector('.current-chain');
+
+    expect(btn?.textContent).toBe('Switch Chain');
+    expect(shownCurrentChain).not.toBeNull();
+
+    fireEvent.click(btn);
+  });
+});
