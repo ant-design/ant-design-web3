fbccc754f9fd550778207c4df5939daff1d1b5e9
愚指导 <yutingzhao1991@gmail.com>
2024-01-18 09:16:05 +0800

    docs: update dev course to wagmi 2 (#506)
    * docs: update dev course to wagmi 2
    
    * fix: add more wallets link error

--- docs/course/demos/connect.tsx
+++ docs/course/demos/connect.tsx
@@ -1,24 +1,26 @@
-import { ConnectButton, Connector } from '@ant-design/web3';
-import { WagmiWeb3ConfigProvider } from '@ant-design/web3-wagmi';
+import { Address, ConnectButton, Connector, NFTCard } from '@ant-design/web3';
+import { MetaMask, WagmiWeb3ConfigProvider } from '@ant-design/web3-wagmi';
 import { createConfig, http } from 'wagmi';
 import { mainnet } from 'wagmi/chains';
 import { injected } from 'wagmi/connectors';
 
 const config = createConfig({
   chains: [mainnet],
+  transports: {
+    [mainnet.id]: http(),
+  },
   connectors: [
     injected({
       target: 'metaMask',
     }),
   ],
-  transports: {
-    [mainnet.id]: http(),
-  },
 });
 
 export default () => {
   return (
-    <WagmiWeb3ConfigProvider config={config}>
+    <WagmiWeb3ConfigProvider config={config} wallets={[MetaMask()]}>
+      <Address format address="0xEcd0D12E21805803f70de03B72B1C162dB0898d9" />
+      <NFTCard address="0x79fcdef22feed20eddacbb2587640e45491b757f" tokenId={8540} />
       <Connector>
         <ConnectButton />
       </Connector>
