8029400a75b0fc64b28f31e038acfb7b9ac10e85
tingzhao.ytz <yutingzhao1991@gmail.com>
2024-01-22 11:38:27 +0800

    chore: use lodash-es

--- packages/common/src/web3-config-provider/index.tsx
+++ packages/common/src/web3-config-provider/index.tsx
@@ -1,5 +1,5 @@
 import React, { useMemo } from 'react';
-import merge from 'lodash.merge';
+import { merge } from 'lodash-es';
 
 import defaultLocale from '../locale/default';
 import { ConfigContext, type ConfigConsumerProps, type Web3ConfigProviderProps } from './context';
