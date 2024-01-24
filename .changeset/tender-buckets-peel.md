3a7688d2284713c455f8e09784b89fc049e52ef6
thinkasany <480968828@qq.com>
2024-01-24 09:53:01 +0800

    test: Increase ConnectModal's components test coverage

---
+++ .changeset/tender-buckets-peel.md
@@ -0,0 +1,5 @@
+---
+'@ant-design/web3': patch
+---
+
+test: Increase ConnectModal's components test coverage
---
+++ packages/web3/src/connect-modal/__tests__/MainPanelHeader.test.tsx
@@ -0,0 +1,39 @@
+import { useContext } from 'react';
+import { fireEvent, render } from '@testing-library/react';
+import { Grid } from 'antd';
+import { describe, expect, it, vi } from 'vitest';
+
+import MainPanelHeader from '../components/MainPanelHeader';
+import { connectModalContext, ConnectModalContextProvider } from '../context';
+
+describe('MainPanelHeader test', async () => {
+  const onPanelRouteBackCallTest = vi.fn();
+  it('When canBack is true, the panelRouteBack function is triggered.', async () => {
+    vi.spyOn(Grid, 'useBreakpoint').mockReturnValue({
+      md: true, // ≥ 768px, mock PC
+    });
+
+    const App = () => {
+      const { prefixCls } = useContext(connectModalContext);
+      return (
+        <div>
+          <ConnectModalContextProvider
+            value={{
+              panelRouteBack: onPanelRouteBackCallTest,
+              prefixCls,
+              canBack: true,
+            }}
+          >
+            <MainPanelHeader title="title" onBack={() => Promise.resolve(true)} />
+          </ConnectModalContextProvider>
+        </div>
+      );
+    };
+    const { baseElement } = render(<App />);
+    const btn = baseElement.querySelector('.ant-web3-connect-modal-main-panel-header-back');
+    fireEvent.click(btn!);
+    await vi.waitFor(() => {
+      expect(onPanelRouteBackCallTest).toBeCalled();
+    });
+  });
+});
