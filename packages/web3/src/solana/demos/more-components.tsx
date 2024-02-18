18d31c7be823f94e4bdc3dcba6a0deebe8aaa9b8
gin-lsl <gin_lsl@outlook.com>
2024-02-18 15:59:55 +0800

    docs: fix Solana NFTCard demo

--- packages/web3/src/solana/demos/more-components.tsx
+++ packages/web3/src/solana/demos/more-components.tsx
@@ -1,18 +1,20 @@
 import { Address, BrowserLink, NFTCard } from '@ant-design/web3';
 import { CoinbaseWallet, SolanaWeb3ConfigProvider } from '@ant-design/web3-solana';
+import { Space } from 'antd';
 
 const App: React.FC = () => {
   return (
-    <SolanaWeb3ConfigProvider wallets={[CoinbaseWallet()]}>
-      <div>
+    <SolanaWeb3ConfigProvider
+      rpcProvider={() =>
+        'https://mainnet.helius-rpc.com/?api-key=cec63e24-d5fd-4e7e-a2ee-5518e08b8485'
+      }
+      wallets={[CoinbaseWallet()]}
+    >
+      <Space direction="vertical">
         <BrowserLink address="GNyDA4Z7vgTzi35qRFHBa4BDjZiugycWhNGWgcJcxiMo" ellipsis />
-      </div>
-
-      <div>
         <Address address="GNyDA4Z7vgTzi35qRFHBa4BDjZiugycWhNGWgcJcxiMo" ellipsis />
-      </div>
-
-      <NFTCard address="GNyDA4Z7vgTzi35qRFHBa4BDjZiugycWhNGWgcJcxiMo" tokenId={3332} />
+        <NFTCard address="GNyDA4Z7vgTzi35qRFHBa4BDjZiugycWhNGWgcJcxiMo" tokenId={3332} />
+      </Space>
     </SolanaWeb3ConfigProvider>
   );
 };
