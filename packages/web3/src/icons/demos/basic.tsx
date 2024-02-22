843d6af11c4c383e8e45d6fca654209a9343161e
gaoyizhuo.gyz <gaoyizhuo.gyz@antgroup.com>
2024-02-22 14:52:45 +0800

    feat: change style to inline

--- packages/web3/src/icons/demos/basic.tsx
+++ packages/web3/src/icons/demos/basic.tsx
@@ -1,4 +1,4 @@
-import { BitcoinCircleColorful, BtczCircleColorful, EthereumFilled } from '@ant-design/web3-icons';
+import { BitcoinCircleColorful, EthereumFilled } from '@ant-design/web3-icons';
 import { Space } from 'antd';
 
 const App: React.FC = () => {
@@ -6,7 +6,6 @@ const App: React.FC = () => {
     <Space wrap>
       <BitcoinCircleColorful />
       <EthereumFilled />
-      <BtczCircleColorful />
     </Space>
   );
 };
