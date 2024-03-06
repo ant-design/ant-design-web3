e8d76af3735b1df73fec18f6fc920f3fa73de0c4
愚指导 <yutingzhao1991@gmail.com>
2024-03-06 15:28:13 +0800

    docs: add a recommentd demo for connect wallet config (#610)
    Co-authored-by: tingzhao.ytz <tingzhao.ytz@antgroup.com>

---
+++ packages/web3/src/wagmi/demos/recommend.tsx
@@ -0,0 +1,53 @@
+import { ConnectButton, Connector } from '@ant-design/web3';
+import {
+  MetaMask,
+  OkxWallet,
+  TokenPocket,
+  WagmiWeb3ConfigProvider,
+  WalletConnect,
+} from '@ant-design/web3-wagmi';
+import { createConfig, http } from 'wagmi';
+import { mainnet } from 'wagmi/chains';
+import { walletConnect } from 'wagmi/connectors';
+
+const config = createConfig({
+  chains: [mainnet],
+  transports: {
+    [mainnet.id]: http(),
+  },
+  connectors: [
+    walletConnect({
+      showQrModal: false,
+      projectId: YOUR_WALLET_CONNET_PROJECT_ID,
+    }),
+  ],
+});
+
+const App: React.FC = () => {
+  return (
+    <WagmiWeb3ConfigProvider
+      eip6963={{
+        autoAddInjectedWallets: true,
+      }}
+      wallets={[
+        MetaMask(),
+        WalletConnect(),
+        TokenPocket({
+          group: 'Popular',
+        }),
+        OkxWallet(),
+      ]}
+      config={config}
+    >
+      <Connector
+        modalProps={{
+          group: false,
+        }}
+      >
+        <ConnectButton />
+      </Connector>
+    </WagmiWeb3ConfigProvider>
+  );
+};
+
+export default App;
