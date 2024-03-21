b195856d1ae12095b3cd39620543e9d3702a87a9
Jeason <me@cowpoke.cc>
2024-03-21 15:57:58 +0800

    chore: use special demos

--- .dumi/theme/builtins/ThemeEditorPage/demos/connect-button.tsx
+++ .dumi/theme/builtins/ThemeEditorPage/demos/connect-button.tsx
@@ -1,33 +1,102 @@
 import React from 'react';
-import { ConnectButton, type Chain } from '@ant-design/web3';
-import { Mainnet, Polygon } from '@ant-design/web3-assets';
-import { Space } from 'antd';
 import type { ComponentDemo } from 'antd-token-previewer-web3';
 
-const Demo = () => {
-  const [chain, setChain] = React.useState<Chain>(Polygon);
-
-  return (
-    <Space>
-      <ConnectButton account={{ address: '3ea2cfd153b8d8505097b81c87c11f5d05097c18' }} tooltip />
-      <ConnectButton
-        chain={chain}
-        availableChains={[Mainnet, Polygon]}
-        account={{ address: '3ea2cfd153b8d8505097b81c87c11f5d05097c18' }}
-        onSwitchChain={async (c) => {
-          setChain(c);
-        }}
-      />
-    </Space>
-  );
+export const components: Record<string, string[]> = {
+  'Connect Button': [
+    'Avatar',
+    'Balance',
+    'Basic',
+    'Chains',
+    'Icon',
+    'Menu',
+    'Name',
+    'Profile Modal',
+    'Quick Connect',
+    'Tooltip',
+    'Type',
+  ],
 };
 
-const demos: ComponentDemo[] = [
-  {
-    demo: <Demo />,
-    tokens: [],
-    key: 'Connect Button',
-  },
-];
-
-export default demos;
+export const demos: Record<string, ComponentDemo[]> = {
+  Avatar: [
+    {
+      demo: React.createElement(require('@ant-design/web3/connect-button/demos/avatar').default),
+      tokens: [],
+      key: 'Avatar',
+    },
+  ],
+  Balance: [
+    {
+      demo: React.createElement(require('@ant-design/web3/connect-button/demos/balance').default),
+      tokens: [],
+      key: 'Balance',
+    },
+  ],
+  Basic: [
+    {
+      demo: React.createElement(require('@ant-design/web3/connect-button/demos/basic').default),
+      tokens: [],
+      key: 'Basic',
+    },
+  ],
+  Chains: [
+    {
+      demo: React.createElement(require('@ant-design/web3/connect-button/demos/chains').default),
+      tokens: [],
+      key: 'Chains',
+    },
+  ],
+  Icon: [
+    {
+      demo: React.createElement(require('@ant-design/web3/connect-button/demos/icon').default),
+      tokens: [],
+      key: 'Icon',
+    },
+  ],
+  Menu: [
+    {
+      demo: React.createElement(require('@ant-design/web3/connect-button/demos/menu').default),
+      tokens: [],
+      key: 'Menu',
+    },
+  ],
+  Name: [
+    {
+      demo: React.createElement(require('@ant-design/web3/connect-button/demos/name').default),
+      tokens: [],
+      key: 'Name',
+    },
+  ],
+  'Profile Modal': [
+    {
+      demo: React.createElement(
+        require('@ant-design/web3/connect-button/demos/profileModal').default,
+      ),
+      tokens: [],
+      key: 'Profile Modal',
+    },
+  ],
+  'Quick Connect': [
+    {
+      demo: React.createElement(
+        require('@ant-design/web3/connect-button/demos/quick-connect').default,
+      ),
+      tokens: [],
+      key: 'Quick Connect',
+    },
+  ],
+  Tooltip: [
+    {
+      demo: React.createElement(require('@ant-design/web3/connect-button/demos/tooltip').default),
+      tokens: [],
+      key: 'Tooltip',
+    },
+  ],
+  Type: [
+    {
+      demo: React.createElement(require('@ant-design/web3/connect-button/demos/type').default),
+      tokens: [],
+      key: 'Type',
+    },
+  ],
+};
