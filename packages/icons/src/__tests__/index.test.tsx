cc2d2a47d5ad8bc410eb8b8c0c667044444fee37
thinkasany <480968828@qq.com>
2024-03-29 18:55:47 +0800

    test(icon): remove unused case (#743)

--- packages/icons/src/__tests__/index.test.tsx
+++ packages/icons/src/__tests__/index.test.tsx
@@ -11,26 +11,4 @@ describe('Test Icons', () => {
       expect(container.firstChild).toMatchSnapshot();
     });
   });
-
-  it('icon path referenced id should start with `ant-web3-icon-`', async () => {
-    const comps = await getComponents('components');
-
-    const relIdRegex = /url\(#[a-zA-Z0-9-]+\)/g;
-
-    comps.forEach((Comp) => {
-      const { container } = render(<Comp />);
-
-      const htmlText = container.innerHTML;
-
-      // find all `url(#id)` in the html text
-      const matches = htmlText.match(relIdRegex);
-
-      matches?.forEach((match) => {
-        expect(
-          match,
-          `${Comp.displayName} reference id: ${match} is not start with ant-web3-icon-`,
-        ).toMatch(/^url\(#ant-web3-icon-/);
-      });
-    });
-  });
 });
