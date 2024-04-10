3486fb9bbb4a397427863d5e92d3b20551ae236e
gin-lsl <gin_lsl@outlook.com>
2024-04-10 13:30:19 +0800

    feat: update built-in wallet

--- packages/solana/src/wallets/built-in.ts
+++ packages/solana/src/wallets/built-in.ts
@@ -1,17 +1,22 @@
 import {
   metadata_CoinbaseWallet,
   metadata_Phantom,
+  metadata_Trust,
   metadata_WalletConnect,
 } from '@ant-design/web3-assets';
-import { CoinbaseWalletAdapter, PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
+import {
+  CoinbaseWalletAdapter,
+  PhantomWalletAdapter,
+  TrustWalletAdapter,
+} from '@solana/wallet-adapter-wallets';
 
 import { WalletConnectWalletAdapter } from '../wallet-connect-adapter';
 import { WalletConnectWalletFactory, WalletFactory } from './factory';
 
 export const CoinbaseWallet = () =>
   WalletFactory(new CoinbaseWalletAdapter(), metadata_CoinbaseWallet);
-
 export const PhantomWallet = () => WalletFactory(new PhantomWalletAdapter(), metadata_Phantom);
+export const TrustWallet = () => WalletFactory(new TrustWalletAdapter(), metadata_Trust);
 
 export const WalletConnectWallet = () =>
   WalletConnectWalletFactory(new WalletConnectWalletAdapter(), metadata_WalletConnect);
