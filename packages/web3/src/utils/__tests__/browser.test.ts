1716c5f834fb6e8bd0aa9b84bc9e845590b5142b
愚指导 <yutingzhao1991@gmail.com>
2024-04-23 20:49:10 +0800

    fix: use copy-to-clipboard and antd Typography.text for fix Address c… (#821)
    * fix: use copy-to-clipboard and antd Typography.text for fix Address copy bug
    
    * fix: remove useless code
    
    ---------
    
    Co-authored-by: tingzhao.ytz <tingzhao.ytz@antgroup.com>

--- packages/web3/src/utils/__tests__/browser.test.ts
+++ packages/web3/src/utils/__tests__/browser.test.ts
@@ -1,10 +1,9 @@
 import { describe, expect, it } from 'vitest';
 
-import { getPlatform, readCopyText, writeCopyText } from '../browser';
-import { browsers, mockBrowser, mockClipboard } from '../test-utils';
+import { getPlatform, writeCopyText } from '../browser';
+import { browsers, mockBrowser, readCopyText } from '../test-utils';
 
 describe('utils/browser', () => {
-  let resetMockClipboard: () => void;
   it('getPlatform', () => {
     Object.keys(browsers).forEach((key) => {
       const reset = mockBrowser(key);
@@ -14,10 +13,8 @@ describe('utils/browser', () => {
     expect(getPlatform()).toBe('Other');
   });
   it('writeCopyText & readCopyText', () => {
-    resetMockClipboard = mockClipboard();
     const test = 'test copy text';
     writeCopyText(test);
     expect(readCopyText()).resolves.toBe(test);
-    resetMockClipboard();
   });
 });
