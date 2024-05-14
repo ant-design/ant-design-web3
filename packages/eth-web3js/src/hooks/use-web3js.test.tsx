8931f7dc47154a6003ae2df2fb79d24b6873bc27
Jeason <me@cowpoke.cc>
2024-05-14 16:35:02 +0800

    test(web3js): fix coverage

--- packages/eth-web3js/src/hooks/use-web3js.test.tsx
+++ packages/eth-web3js/src/hooks/use-web3js.test.tsx
@@ -1,24 +1,27 @@
+import { Mainnet } from '@ant-design/web3-assets';
 import { render } from '@testing-library/react';
-import { describe, expect, test } from 'vitest';
-import Web3 from 'web3';
+import { describe, expect, test, vi } from 'vitest';
 
+import { MetaMask } from '../wallets';
 import { EthWeb3jsConfigProvider } from '../web3js-provider';
 import { useWeb3js } from './use-web3js';
 
 describe('useWeb3js', async () => {
   test('basic usage', async () => {
     const CustomConnector: React.FC = () => {
-      const web3 = useWeb3js();
-      expect(web3).instanceOf(Web3);
-      return null;
+      useWeb3js();
+      return <div className="text">web3js</div>;
     };
 
     const App = () => (
-      <EthWeb3jsConfigProvider>
+      <EthWeb3jsConfigProvider wallets={[MetaMask()]} chains={[Mainnet]}>
         <CustomConnector />
       </EthWeb3jsConfigProvider>
     );
 
-    render(<App />);
+    const { baseElement } = render(<App />);
+    await vi.waitFor(() => {
+      expect(baseElement.querySelector('.text')?.textContent).toBe('web3js');
+    });
   });
 });
