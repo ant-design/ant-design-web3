6a7baab19488a7e554b4029729e113042a8e4728
Jeason <me@cowpoke.cc>
2024-05-14 11:41:30 +0800

    chore(ethers): reset code case by cr comment

--- .dumi/theme/layouts/GlobalLayout.tsx
+++ .dumi/theme/layouts/GlobalLayout.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import { en_US, Web3ConfigProvider, zh_CN } from '@ant-design/web3-common';
-import { ConfigProvider } from 'antd';
 import { useIntl, useLocation, useOutlet, usePrefersColor } from 'dumi';
 import { GlobalLayout as ThemeGlobalLayout } from 'dumi-theme-antd-web3';
 
 import SiteThemeProvider from '../SiteThemeProvider';
-
-// 这里是为了处理一个 dumi 的 bug
-const getPopupContainer = () => document.body;
 
 const GlobalLayout: React.FC = () => {
   const outlet = useOutlet();
@@ -16,18 +12,14 @@ const GlobalLayout: React.FC = () => {
   const { locale } = useIntl();
 
   return (
-    <ConfigProvider getPopupContainer={getPopupContainer}>
-      {/* @ts-ignore ts props error in dumi theme */}
-      <ThemeGlobalLayout>
-        <Web3ConfigProvider locale={locale === 'zh-CN' ? zh_CN : en_US}>
-          <SiteThemeProvider themeMode={color || 'auto'}>
-            <div className={pathname === '/' || pathname === '/index-cn' ? 'home' : ''}>
-              {outlet}
-            </div>
-          </SiteThemeProvider>
-        </Web3ConfigProvider>
-      </ThemeGlobalLayout>
-    </ConfigProvider>
+    // @ts-ignore ts props error in dumi theme
+    <ThemeGlobalLayout>
+      <Web3ConfigProvider locale={locale === 'zh-CN' ? zh_CN : en_US}>
+        <SiteThemeProvider themeMode={color || 'auto'}>
+          <div className={pathname === '/' || pathname === '/index-cn' ? 'home' : ''}>{outlet}</div>
+        </SiteThemeProvider>
+      </Web3ConfigProvider>
+    </ThemeGlobalLayout>
   );
 };
 
