0aff9c6219099ec985efdfb81aac0b9a8894aa49
kiner-tang(文辉) <1127031143@qq.com>
2024-06-02 10:31:22 +0800

    feat: optimize site home page

--- .dumi/theme/builtins/HomePage/components/ShowCase/index.tsx
+++ .dumi/theme/builtins/HomePage/components/ShowCase/index.tsx
@@ -1,3 +1,5 @@
+import { Carousel } from 'antd';
+import classNames from 'classnames';
 import { useIntl } from 'dumi';
 
 import Address from './Address';
@@ -9,17 +11,29 @@ import NFTCard from './NFTCard';
 export const ShowCase: React.FC = () => {
   const intl = useIntl();
 
+  const caseList = (
+    <>
+      <ConnectButton />
+      <Address />
+      <NFTCard />
+      <Connect />
+    </>
+  );
+
   return (
     <div className={styles.container}>
       <h3 className={styles.headTitle}>{intl.formatMessage({ id: 'app.docs.site.case.title' })}</h3>
       <div className={styles.desc}>
         {intl.formatMessage({ id: 'app.docs.site.case.description' })}
       </div>
-      <div className={styles.componentList}>
-        <ConnectButton />
-        <Address />
-        <NFTCard />
-        <Connect />
+      <div className={classNames(styles.componentList, styles.pc)}>{caseList}</div>
+      <div className={classNames(styles.componentList, styles.mobile)}>
+        <Carousel dots={{ className: styles.carouselDot }} dotPosition="top">
+          <ConnectButton />
+          <Address />
+          <NFTCard />
+          <Connect />
+        </Carousel>
       </div>
     </div>
   );
