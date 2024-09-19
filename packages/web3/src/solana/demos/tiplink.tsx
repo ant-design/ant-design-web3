0c51d447736d7fa95327d4c3bc065e03b26af3e5
gin-lsl <gin_lsl@outlook.com>
2024-09-19 21:31:11 +0800

    docs: update tiplink example

--- packages/web3/src/solana/demos/tiplink.tsx
+++ packages/web3/src/solana/demos/tiplink.tsx
@@ -1,29 +1,20 @@
 import React, { useMemo } from 'react';
-import type { WalletMetadata } from '@ant-design/web3';
 import { ConnectButton, Connector } from '@ant-design/web3';
-import { PhantomWallet, SolanaWeb3ConfigProvider, WalletFactory } from '@ant-design/web3-solana';
-import { GoogleViaTipLinkWalletName, TipLinkWalletAdapter } from '@tiplink/wallet-adapter';
-
-const metadata_TipLink: WalletMetadata = {
-  name: GoogleViaTipLinkWalletName,
-  remark: 'TipLink Wallet',
-  app: {
-    link: 'https://tiplink.io/',
-  },
-};
-
-export const TiplinkWallet = () =>
-  WalletFactory(
-    new TipLinkWalletAdapter({
-      clientId: YOUR_TIPLINK_CLIENT_ID,
-      theme: 'system',
-      title: 'Ant Design Web3',
-    }),
-    metadata_TipLink,
-  );
+import { PhantomWallet, SolanaWeb3ConfigProvider, TipLinkWallet } from '@ant-design/web3-solana';
 
 const App: React.FC = () => {
-  const wallets = useMemo(() => [PhantomWallet(), TiplinkWallet()], []);
+  const wallets = useMemo(
+    () => [
+      PhantomWallet(),
+      TipLinkWallet({
+        clientId: YOUR_TIPLINK_CLIENT_ID,
+        theme: 'system',
+        title: 'Ant Design Web3',
+        hideDraggableWidget: true,
+      }),
+    ],
+    [],
+  );
 
   return (
     <SolanaWeb3ConfigProvider wallets={wallets}>
