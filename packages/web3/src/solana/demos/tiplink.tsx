f33957930d1c2dded49328505ecc1870f018002e
gin-lsl <gin_lsl@outlook.com>
2024-09-20 10:55:31 +0800

    docs: update example

--- packages/web3/src/solana/demos/tiplink.tsx
+++ packages/web3/src/solana/demos/tiplink.tsx
@@ -1,23 +1,20 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 import { ConnectButton, Connector } from '@ant-design/web3';
 import { PhantomWallet, SolanaWeb3ConfigProvider, TipLinkWallet } from '@ant-design/web3-solana';
 
 const App: React.FC = () => {
-  const wallets = useMemo(
-    () => [
-      PhantomWallet(),
-      TipLinkWallet({
-        clientId: YOUR_TIPLINK_CLIENT_ID,
-        theme: 'system',
-        title: 'Ant Design Web3',
-        hideDraggableWidget: true,
-      }),
-    ],
-    [],
-  );
-
   return (
-    <SolanaWeb3ConfigProvider wallets={wallets}>
+    <SolanaWeb3ConfigProvider
+      wallets={[
+        PhantomWallet(),
+        TipLinkWallet({
+          clientId: YOUR_TIPLINK_CLIENT_ID,
+          theme: 'system',
+          title: 'Ant Design Web3',
+          hideDraggableWidget: true,
+        }),
+      ]}
+    >
       <Connector>
         <ConnectButton />
       </Connector>
