5a83406cdd8c92d5ee31f1641b3574ffc7184e5e
tingzhao.ytz <tingzhao.ytz@antgroup.com>
2024-09-23 15:38:40 +0800

    docs: update nft image url in demo

--- .dumi/theme/builtins/HomePage/components/ShowCase/NFTCard.tsx
+++ .dumi/theme/builtins/HomePage/components/ShowCase/NFTCard.tsx
@@ -11,7 +11,7 @@ export default () => {
           name="My NFT"
           tokenId={16}
           description="This is description"
-          image="https://api.our-metaverse.xyz/ourms/6_pnghash_0cecc6d080015b34f60bdd253081f36e277ce20ceaf7a6340de3b06d2defad6a_26958469.webp"
+          image="https://mdn.alipayobjects.com/huamei_mutawc/afts/img/A*jlrDRrBXZiEAAAAAAAAAAAAADlrGAQ/original"
         />
       </div>
     </div>
