d1bb1b327c06d5cd5b3d59ec115c29f56ec78804
Likang0122 <abfind@126.com>
2024-11-27 16:30:39 +0800

    fix: rename

--- packages/web3/src/tron/demos/message.tsx
+++ packages/web3/src/tron/demos/message.tsx
@@ -2,7 +2,7 @@ import { ConnectButton, Connector } from '@ant-design/web3';
 import { OkxTronWallet, TronlinkWallet, TronWeb3ConfigProvider } from '@ant-design/web3-tron';
 import { Flex } from 'antd';
 
-import { SignMessage } from './components/message';
+import { SignMessage } from './components/SignMessage';
 
 const Message = () => {
   return (
