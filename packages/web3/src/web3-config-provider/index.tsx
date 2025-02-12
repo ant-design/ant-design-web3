ff4a78002c82a4704565747a971a930ebc9abe5d
tingzhao.ytz <tingzhao.ytz@antgroup.com>
2025-02-12 15:21:34 +0800

    feat: support retro demo theme

--- packages/web3/src/web3-config-provider/index.tsx
+++ packages/web3/src/web3-config-provider/index.tsx
@@ -2,13 +2,11 @@ import {
   Web3ConfigProvider as AntdWeb3ConfigProvider,
   type Web3ConfigProviderProps,
 } from '@ant-design/web3-common';
-import { ConfigProvider, version, type ThemeConfig } from 'antd';
+import { ConfigProvider, type ThemeConfig } from 'antd';
 
 import { ComponentToken as ConnectModalComponentToken } from '../connect-modal/style/index';
 
-console.log('antd version', version);
-
-export interface Web3ThemeConfig extends ThemeConfig {
+interface Web3ThemeConfig extends ThemeConfig {
   web3Components?: {
     ConnectModal?: Partial<ConnectModalComponentToken>;
   };
@@ -33,4 +31,4 @@ const Web3ConfigProvider: React.FC<{ theme?: Web3ThemeConfig } & Web3ConfigProvi
   );
 };
 
-export { Web3ConfigProvider };
+export { Web3ConfigProvider, Web3ThemeConfig };
