5e3b7dc025e0e7afda2ff1c3b817b4a90fc8e809
tingzhao.ytz <yutingzhao1991@gmail.com>
2025-03-06 14:59:29 +0800

    fix: typeerror

--- packages/ton/src/wallets/type.ts
+++ packages/ton/src/wallets/type.ts
@@ -3,7 +3,7 @@ import type { WalletInfo } from '@tonconnect/sdk';
 
 export type TonBasicWallet = TonWalletMetadata &
   WalletInfo & {
-    deeplink?: string;
+    universalLink?: string;
   };
 
 export type TonWallet = Wallet & WalletInfo;
