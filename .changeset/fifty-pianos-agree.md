09565b3b4cb5607106aaef74bdfeda8463081638
LiKang <abfind@126.com>
2025-04-16 17:44:07 +0800

    chore: add wallet extensions info (#1395)
    * chore: add wallet extensions info
    
    * chore: update changelog
    
    ---------
    
    Co-authored-by: tingzhao.ytz <yutingzhao1991@gmail.com>

---
+++ .changeset/fifty-pianos-agree.md
@@ -0,0 +1,5 @@
+---
+'@ant-design/web3-ton': minor
+---
+
+feat: update wallet extension info
--- packages/ton/src/wallets/myTonWallet.tsx
+++ packages/ton/src/wallets/myTonWallet.tsx
@@ -1,8 +1,18 @@
-import { MytonWalletCircleColorful } from '@ant-design/web3-icons';
+import { ChromeCircleColorful, MytonWalletCircleColorful } from '@ant-design/web3-icons';
 
 import type { TonWalletMetadata } from './type';
 
 export const myTonWallet: TonWalletMetadata = {
   key: 'mytonwallet',
   icon: <MytonWalletCircleColorful />,
+  extensions: [
+    {
+      key: 'Chrome',
+      browserIcon: <ChromeCircleColorful />,
+      browserName: 'Chrome',
+      link: 'https://chromewebstore.google.com/detail/fldfpgipfncgndfolcbkdeeknbbbnhcc',
+      description:
+        'The most feature-rich TON extension – with support of multi-accounts, tokens, NFT, TON DNS, TON Sites, TON Proxy, and TON Magic.',
+    },
+  ],
 };
