72205b049fcf594a3e3e409f8c675c352c492d09
yutingzhao1991 <yutingzhao1991@users.noreply.github.com>
2025-05-26 07:01:18 +0000

    deploy: 9be9f6fca0ca4f56080a785d065f6f2811de9eb8

---
+++ docs__guide__changelog.md.b33e1918.async.js
@@ -0,0 +1,26 @@
+"use strict";(self.webpackChunk_ant_design_web3_docs=self.webpackChunk_ant_design_web3_docs||[]).push([[9083],{78612:function(d,a,e){e.r(a);var s=e(52365),h=e(64967),m=e(68795),c=e(34916),g=e(31732),t=e(16028),v=e(77593),x=e(81686),w=e(2721),p=e(42819),b=e(66025),E=e(36458),I=e(25382),j=e(87492),i=e(98712),P=e(62182),D=e(7825),M=e(82750),l=e(44512),o=e(8136),r=e(1735),n=e(99216),_=e(72152);function u(){return(0,_.jsx)(l.DumiPage,{children:(0,_.jsx)(r.Suspense,{fallback:(0,_.jsx)(o.Z,{}),children:(0,_.jsxs)(_.Fragment,{children:[(0,_.jsxs)("div",{className:"markdown",children:[(0,_.jsxs)("h1",{id:"how-to-add-changelog",children:[(0,_.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#how-to-add-changelog",children:(0,_.jsx)("span",{className:"icon icon-link"})}),"How to add Changelog"]}),(0,_.jsxs)("h2",{id:"what-changes-need-a-changelog",children:[(0,_.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#what-changes-need-a-changelog",children:(0,_.jsx)("span",{className:"icon icon-link"})}),"What changes need a Changelog?"]}),(0,_.jsxs)("p",{children:[n.texts[0].value,(0,_.jsx)("code",{children:n.texts[1].value})]}),(0,_.jsxs)("ul",{children:[(0,_.jsx)("li",{children:n.texts[2].value}),(0,_.jsx)("li",{children:n.texts[3].value}),(0,_.jsx)("li",{children:n.texts[4].value}),(0,_.jsx)("li",{children:n.texts[5].value}),(0,_.jsx)("li",{children:n.texts[6].value}),(0,_.jsx)("li",{children:n.texts[7].value})]}),(0,_.jsxs)("h2",{id:"how-to-generate-changelog",children:[(0,_.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#how-to-generate-changelog",children:(0,_.jsx)("span",{className:"icon icon-link"})}),"How to generate changelog"]}),(0,_.jsx)("ol",{children:(0,_.jsx)("li",{children:n.texts[8].value})})]}),(0,_.jsx)(i.Z,{command:"changeset"}),(0,_.jsxs)("div",{className:"markdown",children:[(0,_.jsx)(t.Z,{children:n.texts[9].value}),(0,_.jsx)("ol",{start:"2",children:(0,_.jsx)("li",{children:n.texts[10].value})}),(0,_.jsx)(t.Z,{children:n.texts[11].value}),(0,_.jsx)(t.Z,{children:n.texts[12].value}),(0,_.jsx)("ol",{start:"3",children:(0,_.jsxs)("li",{children:[n.texts[13].value,(0,_.jsx)("code",{children:n.texts[14].value}),n.texts[15].value,(0,_.jsx)("code",{children:n.texts[16].value}),n.texts[17].value]})}),(0,_.jsx)(t.Z,{children:n.texts[18].value}),(0,_.jsx)("ol",{start:"4",children:(0,_.jsxs)("li",{children:[n.texts[19].value,(0,_.jsx)("code",{children:n.texts[20].value}),n.texts[21].value,(0,_.jsx)("a",{href:"https://github.com/conventional-changelog/conventional-changelog/tree/master/packages/conventional-changelog-angular",children:n.texts[22].value}),n.texts[23].value]})}),(0,_.jsx)(t.Z,{children:n.texts[24].value})]})]})})})}a.default=u},99216:function(d,a,e){e.r(a),e.d(a,{texts:function(){return s}});const s=[{value:"Modified files under ",paraId:0,tocIndex:1},{value:"packages",paraId:0,tocIndex:1},{value:"@ant-design/web3-assets",paraId:1,tocIndex:1},{value:"@ant-design/web3-common",paraId:1,tocIndex:1},{value:"@ant-design/web3-icons",paraId:1,tocIndex:1},{value:"@ant-design/web3-solana",paraId:1,tocIndex:1},{value:"@ant-design/web3-wagmi",paraId:1,tocIndex:1},{value:"@ant-design/web3",paraId:1,tocIndex:1},{value:"First of all, you need to run the command:",paraId:2,tocIndex:2},{value:`MacBook-Pro ant-design-web3 % pnpm changeset
+
+> @ant-design/web3-docs@0.0.0 changeset ant-design-web3
+> changeset
+`,paraId:3},{value:"Select the modified package by space and press enter to confirm\u3002",paraId:4},{value:`\u{1F98B}  Which packages would you like to include? \u2026
+\u25C9 changed packages
+  \u25C9 @ant-design/web3
+\u25EF unchanged packages
+  \u25EF @ant-design/web3-assets
+  \u25EF @ant-design/web3-common
+  \u25EF @ant-design/web3-icons
+  \u25EF @ant-design/web3-solana
+  \u25EF @ant-design/web3-wagmi
+`,paraId:5},{value:`\u{1F98B}  Which packages would you like to include? \xB7 @ant-design/web3
+\u{1F98B}  Which packages should have a major bump? \u2026
+\u25EF all packages
+  \u25EF @ant-design/web3@1.9.0
+`,paraId:6},{value:"At this point, we should skip it with a carriage return. For bugfixes in general, we select the last ",paraId:7},{value:"patch",paraId:7},{value:" version, and we select ",paraId:7},{value:"minor",paraId:7},{value:" for newer features.\u3002",paraId:7},{value:`\u{1F98B}  Which packages should have a major bump? \xB7 No items were selected
+\u{1F98B}  Which packages should have a minor bump? \xB7 No items were selected
+\u{1F98B}  The following packages will be patch bumped:
+\u{1F98B}  @ant-design/web3@1.9.0
+\u{1F98B}  Please enter a summary for this change (this will be in the changelogs).
+\u{1F98B}    (submit empty line to open external editor)
+\u{1F98B}  Summary \u203A
+`,paraId:8},{value:"As a final step, we submit message in the ",paraId:9},{value:"Summary",paraId:9},{value:" that conforms to ",paraId:9},{value:"Angular's commit convention",paraId:9},{value:".",paraId:9},{value:`\u{1F98B}  Summary \u203A feat: support more icons
+`,paraId:10}]}}]);
