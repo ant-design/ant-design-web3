6736f205a7cfca4b3cdbc087d0fe75724fa07242
yutingzhao1991 <yutingzhao1991@users.noreply.github.com>
2025-07-28 07:52:28 +0000

    deploy: 639f8f5f55e1ae144cfeb93f51443e0bdae74ec2

---
+++ docs__course__dev-connect.md.fbf78a61.async.js
@@ -0,0 +1,75 @@
+"use strict";(self.webpackChunk_ant_design_web3_docs=self.webpackChunk_ant_design_web3_docs||[]).push([[2195],{23119:function(_,a,t){t.r(a);var o=t(69506),u=t(1110),c=t(37866),x=t(66791),h=t(56368),s=t(5751),m=t(67008),v=t(71282),p=t(70395),I=t(68748),g=t(10632),f=t(17052),w=t(25382),b=t(87492),j=t(98712),E=t(62182),C=t(7825),P=t(82750),d=t(44512),i=t(8136),r=t(1735),n=t(26165),e=t(72152);function l(){return(0,e.jsx)(d.DumiPage,{children:(0,e.jsx)(r.Suspense,{fallback:(0,e.jsx)(i.Z,{}),children:(0,e.jsxs)(e.Fragment,{children:[(0,e.jsxs)("div",{className:"markdown",children:[(0,e.jsxs)("h1",{id:"connect-to-blockchain",children:[(0,e.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#connect-to-blockchain",children:(0,e.jsx)("span",{className:"icon icon-link"})}),"Connect to blockchain"]}),(0,e.jsxs)("h2",{id:"overview",children:[(0,e.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#overview",children:(0,e.jsx)("span",{className:"icon icon-link"})}),"Overview"]}),(0,e.jsx)("p",{children:n.texts[0].value}),(0,e.jsxs)("h2",{id:"install-dependencies",children:[(0,e.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#install-dependencies",children:(0,e.jsx)("span",{className:"icon icon-link"})}),"Install dependencies"]}),(0,e.jsxs)("p",{children:[n.texts[1].value,(0,e.jsx)(d.Link,{to:"/guide/quick-start",children:n.texts[2].value}),n.texts[3].value,(0,e.jsx)("code",{children:n.texts[4].value}),n.texts[5].value,(0,e.jsx)("code",{children:n.texts[6].value}),n.texts[7].value,(0,e.jsx)("code",{children:n.texts[8].value}),n.texts[9].value,(0,e.jsx)("code",{children:n.texts[10].value}),n.texts[11].value]}),(0,e.jsx)(s.Z,{lang:"shell",children:n.texts[12].value}),(0,e.jsxs)("p",{children:[(0,e.jsx)("code",{children:n.texts[13].value}),n.texts[14].value,(0,e.jsx)(d.Link,{to:"/guide/adapter",children:n.texts[15].value}),n.texts[16].value,(0,e.jsx)("a",{href:"https://ethereum.org/",children:n.texts[17].value}),n.texts[18].value,(0,e.jsx)(d.Link,{to:"/components/ethereum",children:n.texts[19].value}),n.texts[20].value]}),(0,e.jsx)("blockquote",{children:(0,e.jsxs)("p",{children:[(0,e.jsx)("a",{href:"https://wagmi.sh/",children:n.texts[21].value}),n.texts[22].value,(0,e.jsx)("code",{children:n.texts[23].value}),n.texts[24].value,(0,e.jsx)("code",{children:n.texts[25].value}),n.texts[26].value]})}),(0,e.jsxs)("h2",{id:"configure-the-adapter",children:[(0,e.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#configure-the-adapter",children:(0,e.jsx)("span",{className:"icon icon-link"})}),"Configure the adapter"]}),(0,e.jsxs)("p",{children:[n.texts[27].value,(0,e.jsx)("a",{href:"https://wagmi.sh/core/getting-started",children:n.texts[28].value}),n.texts[29].value]}),(0,e.jsxs)("p",{children:[n.texts[30].value,(0,e.jsx)("code",{children:n.texts[31].value}),n.texts[32].value]}),(0,e.jsx)(s.Z,{lang:"diff",children:n.texts[33].value}),(0,e.jsxs)("p",{children:[(0,e.jsx)("a",{href:"https://web3.ant.design/components/wagmi-cn#wagmiweb3configproviderprops",children:n.texts[34].value}),n.texts[35].value,(0,e.jsx)("a",{href:"https://wagmi.sh/react/config",children:n.texts[36].value}),n.texts[37].value,(0,e.jsx)("a",{href:"https://web3.ant.design/components/ethereum-cn#%E8%87%AA%E5%AE%9A%E4%B9%89-wagmi-%E9%85%8D%E7%BD%AE",children:n.texts[38].value}),n.texts[39].value,(0,e.jsx)("code",{children:n.texts[40].value}),n.texts[41].value]}),(0,e.jsxs)("p",{children:[n.texts[42].value,(0,e.jsx)("code",{children:n.texts[43].value}),n.texts[44].value]}),(0,e.jsx)(s.Z,{lang:"diff",children:n.texts[45].value}),(0,e.jsx)("p",{children:n.texts[46].value}),(0,e.jsxs)("p",{children:[n.texts[47].value,(0,e.jsx)("code",{children:n.texts[48].value}),n.texts[49].value]}),(0,e.jsx)(s.Z,{lang:"diff",children:n.texts[50].value}),(0,e.jsxs)("p",{children:[(0,e.jsx)("code",{children:n.texts[51].value}),n.texts[52].value,(0,e.jsx)("code",{children:n.texts[53].value}),n.texts[54].value,(0,e.jsx)("code",{children:n.texts[55].value}),n.texts[56].value]}),(0,e.jsx)("p",{children:n.texts[57].value}),(0,e.jsx)("p",{children:(0,e.jsx)("img",{alt:"",src:t(20619)})}),(0,e.jsx)("p",{children:n.texts[58].value}),(0,e.jsxs)("h2",{id:"config-wallet",children:[(0,e.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#config-wallet",children:(0,e.jsx)("span",{className:"icon icon-link"})}),"Config wallet"]}),(0,e.jsxs)("p",{children:[n.texts[59].value,(0,e.jsx)("code",{children:n.texts[60].value}),n.texts[61].value,(0,e.jsx)("code",{children:n.texts[62].value}),n.texts[63].value]}),(0,e.jsxs)("p",{children:[n.texts[64].value,(0,e.jsx)("a",{href:"https://metamask.io/",children:n.texts[65].value}),n.texts[66].value,(0,e.jsx)("code",{children:n.texts[67].value}),n.texts[68].value,(0,e.jsx)("code",{children:n.texts[69].value}),n.texts[70].value,(0,e.jsx)("code",{children:n.texts[71].value}),n.texts[72].value]}),(0,e.jsx)(s.Z,{lang:"diff",children:n.texts[73].value}),(0,e.jsx)("p",{children:n.texts[74].value})]}),(0,e.jsx)(d.DumiDemo,{demo:{id:"docs-course-dev-connect-demo-connect"},previewerProps:{filename:"docs/course/demos/connect.tsx"}}),(0,e.jsxs)("div",{className:"markdown",children:[(0,e.jsx)("p",{children:n.texts[75].value}),(0,e.jsx)("p",{children:(0,e.jsx)("img",{alt:"",src:t(72514)})}),(0,e.jsxs)("p",{children:[n.texts[76].value,(0,e.jsx)(d.Link,{to:"/components/ethereum#add-more-wallets",children:n.texts[77].value}),n.texts[78].value]})]})]})})})}a.default=l},26165:function(_,a,t){t.r(a),t.d(a,{texts:function(){return o}});const o=[{value:"In this part of the course, we will guide you to connect the DApp to the blockchain through the wallet and the blockchain. We will take Ethereum as an example to connect to the blockchain and obtain the user's account information.",paraId:0,tocIndex:1},{value:"First, similar to the ",paraId:1,tocIndex:2},{value:"Quick Start",paraId:2,tocIndex:2},{value:" documentation guide, we need to install some dependencies. In the previous course, we have installed ",paraId:1,tocIndex:2},{value:"antd",paraId:1,tocIndex:2},{value:" and ",paraId:1,tocIndex:2},{value:"@ant-design/web3",paraId:1,tocIndex:2},{value:", so we only need to install ",paraId:1,tocIndex:2},{value:"@ant-design/web3-wagmi",paraId:1,tocIndex:2},{value:" and ",paraId:1,tocIndex:2},{value:"wagmi",paraId:1,tocIndex:2},{value:" next.",paraId:1,tocIndex:2},{value:`npm i @ant-design/web3-wagmi wagmi viem @tanstack/react-query --save
+`,paraId:3,tocIndex:2},{value:"@ant-design/web3",paraId:4,tocIndex:2},{value:" is a UI component library, which connects to different blockchains through different ",paraId:4,tocIndex:2},{value:"adapters",paraId:5,tocIndex:2},{value:". In this course, we mainly use ",paraId:4,tocIndex:2},{value:"Ethereum",paraId:4,tocIndex:2},{value:" as an example. Correspondingly, we will also use the ",paraId:4,tocIndex:2},{value:"adapter of Ethereum",paraId:6,tocIndex:2},{value:" to implement the requirements of the course.",paraId:4,tocIndex:2},{value:"wagmi",paraId:7,tocIndex:2},{value:" is an open source React Hooks library that serves Ethereum. The adapter ",paraId:7,tocIndex:2},{value:"@ant-design/web3-wagmi",paraId:7,tocIndex:2},{value:" of Ant Design Web3 is based on it. In the later part of this course, unless otherwise specified, the adapter mentioned refers to ",paraId:7,tocIndex:2},{value:"@ant-design/web3-wagmi",paraId:7,tocIndex:2},{value:".",paraId:7,tocIndex:2},{value:"The configuration of the adapter directly uses the ",paraId:8,tocIndex:3},{value:"official configuration of wagmi",paraId:8,tocIndex:3},{value:". For actual projects, you usually need to configure the JSON RPC address and various wallets. In this course, we will first use the simplest configuration, and then guide you to understand the configuration you need in your actual project.",paraId:8,tocIndex:3},{value:"First, please continue to edit the ",paraId:9,tocIndex:3},{value:"pages/web3.tsx",paraId:9,tocIndex:3},{value:" file and introduce the content you need:",paraId:9,tocIndex:3},{value:`+ import { WagmiWeb3ConfigProvider } from '@ant-design/web3-wagmi';
+import { Address } from "@ant-design/web3";
+
+export default function Web3() {
+  return (
+    <Address format address="0xEcd0D12E21805803f70de03B72B1C162dB0898d9" />
+  );
+};
+`,paraId:10,tocIndex:3},{value:"WagmiWeb3ConfigProvider",paraId:11,tocIndex:3},{value:" is an Ant Design Web3 adapter based on wagmi, used for adapting Ethereum. In addition to adapting wagmi's API to the API required by Ant Design Web3, it also makes some usability improvements. You no longer need to use wagmi's ",paraId:11,tocIndex:3},{value:"createConfig",paraId:11,tocIndex:3},{value:" to create a configuration; it will automatically create the wagmi configuration for you. Of course, if you have special needs, you can also ",paraId:11,tocIndex:3},{value:"customize the configuration",paraId:11,tocIndex:3},{value:", and the customized configuration will override the automatically generated configuration of ",paraId:11,tocIndex:3},{value:"WagmiWeb3ConfigProvider",paraId:11,tocIndex:3},{value:".",paraId:11,tocIndex:3},{value:"Then import the ",paraId:12,tocIndex:3},{value:"WagmiWeb3ConfigProvider",paraId:12,tocIndex:3},{value:":",paraId:12,tocIndex:3},{value:`import { createConfig, http } from "wagmi";
+import { mainnet } from "wagmi/chains";
+import { WagmiWeb3ConfigProvider } from "@ant-design/web3-wagmi";
+import { Address } from "@ant-design/web3";
+
+export default function Web3() {
+  return (
++     <WagmiWeb3ConfigProvider>
+      <div
+        style={{
+          height: "100vh",
+          padding: 64,
+        }}
+      >
+        <Address format address="0xEcd0D12E21805803f70de03B72B1C162dB0898d9" />
+      </div>
++    </WagmiWeb3ConfigProvider>
+  );
+};
+
+`,paraId:13,tocIndex:3},{value:"In this way, we have completed the basic configuration of wagmi, and then we can use Ant Design Web3 components to obtain data on the chain.",paraId:14,tocIndex:3},{value:"Let's us try the ",paraId:15,tocIndex:3},{value:"NFTCard",paraId:15,tocIndex:3},{value:" component.",paraId:15,tocIndex:3},{value:`import { WagmiWeb3ConfigProvider } from "@ant-design/web3-wagmi";
+- import { Address } from "@ant-design/web3";
++ import { Address, NFTCard } from "@ant-design/web3";
+
+export default function Web3() {
+  return (
+    <WagmiWeb3ConfigProvider>
+      <div
+        style={{
+          height: "100vh",
+          padding: 64,
+        }}
+      >
+         <Address format address="0xEcd0D12E21805803f70de03B72B1C162dB0898d9" />
++        <NFTCard address="0xEcd0D12E21805803f70de03B72B1C162dB0898d9" tokenId={641} />
+      </div>
+    </WagmiWeb3ConfigProvider>
+  );
+};
+`,paraId:16,tocIndex:3},{value:"NFTCard",paraId:17,tocIndex:3},{value:" is a component that displays NFT information. We will use it to display the NFT information of the address ",paraId:17,tocIndex:3},{value:"0xEcd0D12E21805803f70de03B72B1C162dB0898d9",paraId:17,tocIndex:3},{value:" and the token ID ",paraId:17,tocIndex:3},{value:"641",paraId:17,tocIndex:3},{value:".",paraId:17,tocIndex:3},{value:"Result:",paraId:18,tocIndex:3},{value:"Please check your network if the result is not displayed.",paraId:19,tocIndex:3},{value:"In the previous section, we have successfully connected to the blockchain and obtained the data on the chain. However, we can see that the address displayed in the ",paraId:20,tocIndex:4},{value:"Address",paraId:20,tocIndex:4},{value:" component is ",paraId:20,tocIndex:4},{value:"0xEcd0D12E21805803f70de03B72B1C162dB0898d9",paraId:20,tocIndex:4},{value:", which is not the address of the current wallet. In this section, we will guide you to connect to the wallet and obtain the address of the current wallet.",paraId:20,tocIndex:4},{value:"We use ",paraId:21,tocIndex:4},{value:"MetaMask",paraId:21,tocIndex:4},{value:" as an example to see how to configure the wallet. Additionally, we have configured ",paraId:21,tocIndex:4},{value:"eip6963",paraId:21,tocIndex:4},{value:"'s ",paraId:21,tocIndex:4},{value:"autoAddInjectedWallets",paraId:21,tocIndex:4},{value:" to ",paraId:21,tocIndex:4},{value:"true",paraId:21,tocIndex:4},{value:", which will automatically add the wallets installed in your browser.",paraId:21,tocIndex:4},{value:`- import { WagmiWeb3ConfigProvider } from "@ant-design/web3-wagmi";
++ import { WagmiWeb3ConfigProvider, MetaMask } from "@ant-design/web3-wagmi";
+- import { Address, NFTCard } from "@ant-design/web3";
++ import { Address, NFTCard, Connector, ConnectButton } from "@ant-design/web3";
+
+export default function Web3() {
+  return (
+-   <WagmiWeb3ConfigProvider>
++    <WagmiWeb3ConfigProvider
++      eip6963={{
++        autoAddInjectedWallets: true,
++      }}
++      wallets={[MetaMask()]}
++    >
+      <Address format address="0xEcd0D12E21805803f70de03B72B1C162dB0898d9" />
+      <NFTCard
+        address="0xEcd0D12E21805803f70de03B72B1C162dB0898d9"
+        tokenId={641}
+      />
++       <Connector>
++         <ConnectButton />
++       </Connector>
+    </WagmiWeb3ConfigProvider>
+  );
+};
+
+`,paraId:22,tocIndex:4},{value:"The full code is as follows:",paraId:23,tocIndex:4},{value:"The result is as follows:",paraId:24},{value:"You can try to refer to ",paraId:25},{value:"this document",paraId:26},{value:" to configure support for more wallets.",paraId:25}]},72514:function(_,a,t){_.exports=t.p+"static/connect.5fae637a.png"},20619:function(_,a,t){_.exports=t.p+"static/nft-card.912cbc6c.png"}}]);
